e6c8971be3b925ae326b26a26bf2e22701fdb607
Kouhei Sutou <kou@clear-code.com>
2015-02-06 00:25:00 +0900

    Add a test for "ORDER BY DESC"

---
+++ sql/compare/integer/order_by_limit/desc.sql
@@ -0,0 +1,26 @@
+CREATE TABLE ids (
+  id integer
+);
+
+INSERT INTO ids VALUES (2);
+INSERT INTO ids VALUES (7);
+INSERT INTO ids VALUES (6);
+INSERT INTO ids VALUES (4);
+INSERT INTO ids VALUES (5);
+INSERT INTO ids VALUES (8);
+INSERT INTO ids VALUES (1);
+INSERT INTO ids VALUES (10);
+INSERT INTO ids VALUES (3);
+INSERT INTO ids VALUES (9);
+
+CREATE INDEX grnindex ON ids USING pgroonga (id);
+
+SET enable_seqscan = off;
+SET enable_indexscan = on;
+
+SELECT id
+  FROM ids
+  ORDER BY id DESC
+  LIMIT 5;
+
+DROP TABLE ids;
