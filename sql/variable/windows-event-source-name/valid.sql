a0556e2122fb79d626043fa73d04be3557d723b1
Kouhei Sutou <kou@clear-code.com>
2015-07-27 10:48:25 +0900

    Revert "Add pgroonga.windows_event_source_name"
    This reverts commit a6f9094b4d48f52fab6e55ddcd3c09390880eb72.
    
    We should use "Windows Event Log"(*1) API rather than
    "Event Logging"(*2) API. Because "Windows Event Log" API is the
    replacement API for "Event Logging" API.
    
    (*1) https://msdn.microsoft.com/en-us/library/windows/desktop/aa385780%28v=vs.85%29.aspx
    (*2) https://msdn.microsoft.com/en-us/library/windows/desktop/aa363652%28v=vs.85%29.aspx

--- sql/variable/windows-event-source-name/valid.sql
+++
@@ -1,8 +0,0 @@
--- To load PGroonga
-SELECT pgroonga.command('status')::json->0->0;
-
-SHOW pgroonga.windows_event_source_name;
-SET pgroonga.windows_event_source_name = 'Groonga';
-SHOW pgroonga.windows_event_source_name;
-SET pgroonga.windows_event_source_name = default;
-SHOW pgroonga.windows_event_source_name;
