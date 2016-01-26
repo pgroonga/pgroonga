4e07aa5c5cae97fa05fcc48a5d2c580243220db3
Kouhei Sutou <kou@clear-code.com>
2016-01-26 22:52:42 +0900

    test: remove needless index definition

--- sql/jsonb/value/boolean/seqscan.sql
+++ sql/jsonb/value/boolean/seqscan.sql
@@ -7,8 +7,6 @@ INSERT INTO fruits VALUES (1, '{"apple":  true}');
 INSERT INTO fruits VALUES (2, '{"banana": false}');
 INSERT INTO fruits VALUES (3, '{"peach":  true}');
 
-CREATE INDEX pgroonga_index ON fruits USING pgroonga (items);
-
 SET enable_seqscan = on;
 SET enable_indexscan = off;
 SET enable_bitmapscan = off;
