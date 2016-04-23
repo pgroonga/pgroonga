92a6aa615038511797ae852eeaf018902f8cd2e8
Kouhei Sutou <kou@clear-code.com>
2016-04-24 00:58:13 +0900

    Add missing prototype

--- src/pgroonga.h
+++ src/pgroonga.h
@@ -58,6 +58,7 @@ extern Datum PGDLLEXPORT pgroonga_command(PG_FUNCTION_ARGS);
 extern Datum PGDLLEXPORT pgroonga_snippet_html(PG_FUNCTION_ARGS);
 extern Datum PGDLLEXPORT pgroonga_highlight_html(PG_FUNCTION_ARGS);
 extern Datum PGDLLEXPORT pgroonga_match_positions_byte(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT pgroonga_query_extract_keywords(PG_FUNCTION_ARGS);
 
 extern Datum PGDLLEXPORT pgroonga_match_term_text(PG_FUNCTION_ARGS);
 extern Datum PGDLLEXPORT pgroonga_match_term_text_array(PG_FUNCTION_ARGS);
