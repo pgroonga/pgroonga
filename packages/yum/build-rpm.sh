658fb058d2b2ba1e51c6b37fd573f0f19bd5d1e6
Kouhei Sutou <kou@clear-code.com>
2016-09-29 15:39:34 +0900

    package yum: update pgdg RPM URL

--- packages/yum/build-rpm.sh
+++ packages/yum/build-rpm.sh
@@ -28,7 +28,9 @@ case "${architecture}" in
     ;;
 esac
 
-run rpm -ivh http://yum.postgresql.org/${PG_VERSION}/redhat/rhel-${distribution_version}-${architecture}/pgdg-centos${PG_PACKAGE_VERSION}-${PG_VERSION}-1.noarch.rpm
+pgdg_rpm=pgdg-centos${PG_PACKAGE_VERSION}-${PG_VERSION}-3.noarch.rpm
+run wget --no-check-certificate https://yum.postgresql.org/${PG_VERSION}/redhat/rhel-${distribution_version}-${architecture}/${pgdg_rpm}
+run rpm -ivh ${pgdg_rpm}
 run rpm -ivh http://packages.groonga.org/centos/groonga-release-1.1.0-1.noarch.rpm
 run yum makecache
 
