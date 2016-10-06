0d1591b9ea268d36705e2afbc478def676c2b3b8
Kouhei Sutou <kou@clear-code.com>
2016-10-07 01:24:55 +0900

    test: add missing clean up

--- sql/index-scan/kill-prior-tuple.sql
+++ sql/index-scan/kill-prior-tuple.sql
@@ -34,3 +34,5 @@ UPDATE ids SET memo = 'b' WHERE id = 10;
 UPDATE ids SET memo = 'b' WHERE id = 5;
 UPDATE ids SET memo = 'b' WHERE id = 6;
 SELECT * FROM ids WHERE 2 <= id and memo @@ 'a';
+
+DROP TABLE ids;
