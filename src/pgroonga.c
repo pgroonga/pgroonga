51ed6e487e289c73d166f009e92466c419f23c36
Kouhei Sutou <kou@clear-code.com>
2016-10-08 22:11:02 +0900

    Extract common code

--- src/pgroonga.c
+++ src/pgroonga.c
@@ -3274,6 +3274,26 @@ PGrnEnsureCursorOpened(IndexScanDesc scan,
 	}
 }
 
+static grn_id
+PGrnScanOpaqueResolveID(PGrnScanOpaque so)
+{
+	grn_id recordID = so->currentID;
+
+	if (so->sorted)
+	{
+		GRN_BULK_REWIND(&(buffers->general));
+		grn_obj_get_value(ctx, so->sorted, recordID, &(buffers->general));
+		recordID = GRN_RECORD_VALUE(&(buffers->general));
+	}
+	if (so->searched)
+	{
+		grn_table_get_key(ctx, so->searched, recordID,
+						  &recordID, sizeof(grn_id));
+	}
+
+	return recordID;
+}
+
 #ifdef PGRN_SUPPORT_INDEX_ONLY_SCAN
 static void
 PGrnGetTupleFillIndexTuple(PGrnScanOpaque so,
@@ -3291,18 +3311,7 @@ PGrnGetTupleFillIndexTuple(PGrnScanOpaque so,
 	values = palloc(sizeof(Datum) * desc->natts);
 	isNulls = palloc(sizeof(bool) * desc->natts);
 
-	recordID = so->currentID;
-	if (so->sorted)
-	{
-		GRN_BULK_REWIND(&(buffers->general));
-		grn_obj_get_value(ctx, so->sorted, recordID, &(buffers->general));
-		recordID = GRN_RECORD_VALUE(&(buffers->general));
-	}
-	if (so->searched)
-	{
-		grn_table_get_key(ctx, so->searched, recordID,
-						  &recordID, sizeof(grn_id));
-	}
+	recordID = PGrnScanOpaqueResolveID(so);
 
 	for (i = 0; i < desc->natts; i++)
 	{
@@ -3338,18 +3347,9 @@ pgroonga_gettuple_raw(IndexScanDesc scan,
 
 	if (scan->kill_prior_tuple && so->currentID != GRN_ID_NIL)
 	{
-		grn_id recordID = so->currentID;
-		if (so->sorted)
-		{
-			GRN_BULK_REWIND(&(buffers->general));
-			grn_obj_get_value(ctx, so->sorted, recordID, &(buffers->general));
-			recordID = GRN_RECORD_VALUE(&(buffers->general));
-		}
-		if (so->searched)
-		{
-			grn_table_get_key(ctx, so->searched, recordID,
-							  &recordID, sizeof(grn_id));
-		}
+		grn_id recordID;
+
+		recordID = PGrnScanOpaqueResolveID(so);
 		grn_table_delete_by_id(ctx, so->sourcesTable, recordID);
 	}
 
