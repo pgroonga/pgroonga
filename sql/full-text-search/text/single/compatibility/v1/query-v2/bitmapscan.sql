dbfe389bd8c7dc8709f8ec4470d75b6c76e510b0
Kouhei Sutou <kou@clear-code.com>
2017-05-01 18:17:53 +0900

    test: add tests for $? with v1 operator class

---
+++ sql/full-text-search/text/single/compatibility/v1/query-v2/bitmapscan.sql
@@ -0,0 +1,21 @@
+CREATE TABLE memos (
+  id integer,
+  content text
+);
+
+INSERT INTO memos VALUES (1, 'PostgreSQL is a RDBMS.');
+INSERT INTO memos VALUES (2, 'Groonga is fast full text search engine.');
+INSERT INTO memos VALUES (3, 'PGroonga is a PostgreSQL extension that uses Groonga.');
+
+CREATE INDEX grnindex ON memos
+ USING pgroonga (content pgroonga.text_full_text_search_ops);
+
+SET enable_seqscan = off;
+SET enable_indexscan = off;
+SET enable_bitmapscan = on;
+
+SELECT id, content
+  FROM memos
+ WHERE content &? 'rdbms OR engine';
+
+DROP TABLE memos;
