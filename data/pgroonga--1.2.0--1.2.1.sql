6d1e2c955bdabc7baca872c0a9d2267e007a82fb
Kouhei Sutou <kou@clear-code.com>
2017-05-02 11:39:34 +0900

    Add pgroonga.text_array_full_text_search_ops_v2 to v1 compatible operators

--- data/pgroonga--1.2.0--1.2.1.sql
+++ data/pgroonga--1.2.0--1.2.1.sql
@@ -128,6 +128,8 @@ CREATE OPERATOR &?> (
 CREATE OPERATOR CLASS pgroonga.text_array_full_text_search_ops_v2
 	FOR TYPE text[]
 	USING pgroonga AS
+		OPERATOR 8 %% (text[], text), -- For backward compatibility
+		OPERATOR 9 @@ (text[], text), -- For backward compatibility
 		OPERATOR 12 &@ (text[], text),
 		OPERATOR 13 &? (text[], text),
 		OPERATOR 14 &~? (text[], text),
