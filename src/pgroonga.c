d067f812354ecf1ad5c77f04c43b50fcaceeeff2
Kouhei Sutou <kou@clear-code.com>
2017-06-19 11:48:17 +0900

    Improve performance for operator against empty text[] value

--- src/pgroonga.c
+++ src/pgroonga.c
@@ -1531,6 +1531,9 @@ pgroonga_execute_binary_operator_in_string_array(ArrayType *operands1,
 {
 	int i, n;
 
+	if (ARR_NDIM(operands1) == 0)
+		return false;
+
 	n = ARR_DIMS(operands1)[0];
 	for (i = 1; i <= n; i++)
 	{
