24a1fa0714b503e8d5814c3a0b20ecd7fde68898
Kouhei Sutou <kou@clear-code.com>
2017-06-20 12:57:40 +0900

    test: stable

--- sql/term-search/text/options/tokenizer/ignored.sql
+++ sql/term-search/text/options/tokenizer/ignored.sql
@@ -6,6 +6,9 @@ CREATE INDEX pgrn_index ON tags
   USING pgroonga (name pgroonga.text_term_search_ops_v2)
   WITH (tokenizer = "TokenDelimit");
 
-SELECT pgroonga.command('table_list')::json#>>'{1,2,6}';
+SELECT entry->>6 AS tokenizer
+  FROM jsonb_array_elements((pgroonga.command('table_list')::jsonb#>'{1}') - 0)
+       AS entry
+ WHERE entry->>1 = 'Lexicon' || 'pgrn_index'::regclass::oid || '_0';
 
 DROP TABLE tags;
