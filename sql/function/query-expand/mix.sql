6b7f043126a32bd99df61e6d628af1484948c45f
Kouhei Sutou <kou@clear-code.com>
2017-06-30 11:24:56 +0900

    pgroonga_escape test: add existent and nonexistent mix case

---
+++ sql/function/query-expand/mix.sql
@@ -0,0 +1,10 @@
+CREATE TABLE synonyms (
+  term text PRIMARY KEY,
+  synonyms text[]
+);
+
+INSERT INTO synonyms VALUES ('Mroonga', ARRAY['Mroonga', 'Groonga MySQL']);
+INSERT INTO synonyms VALUES ('PGroonga', ARRAY['PGroonga', 'Groonga PostgreSQL']);
+
+SELECT pgroonga.query_expand('synonyms', 'term', 'synonyms',
+                             'Ruby (Mroonga OR PGroonga)');
