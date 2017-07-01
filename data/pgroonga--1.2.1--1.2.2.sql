300bf546d30c8075446bbda3551f8b98426456e9
Kouhei Sutou <kou@clear-code.com>
2017-07-01 13:17:37 +0900

    Upgradable

--- data/pgroonga--1.2.1--1.2.2.sql
+++ data/pgroonga--1.2.1--1.2.2.sql
@@ -1,4 +1,7 @@
-CREATE FUNCTION pgroonga.query_expand(term text)
+CREATE FUNCTION pgroonga.query_expand(tableName cstring,
+				      termColumnName text,
+				      synonymsColumnName text,
+				      term text)
 	RETURNS text
 	AS 'MODULE_PATHNAME', 'pgroonga_query_expand'
 	LANGUAGE C
