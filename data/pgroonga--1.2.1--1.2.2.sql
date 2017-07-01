a9d34655c3af0e3c833f6fef49ec30c19e1d46c7
Kouhei Sutou <kou@clear-code.com>
2017-07-01 13:36:52 +0900

    text_term_search_v2: support comparison operators

--- data/pgroonga--1.2.1--1.2.2.sql
+++ data/pgroonga--1.2.1--1.2.2.sql
@@ -7,3 +7,11 @@ CREATE FUNCTION pgroonga.query_expand(tableName cstring,
 	LANGUAGE C
 	STABLE
 	STRICT;
+
+ALTER OPERATOR FAMILY pgroonga.text_term_search_ops_v2 USING pgroonga
+	ADD
+		OPERATOR 1 < (text, text),
+		OPERATOR 2 <= (text, text),
+		OPERATOR 3 = (text, text),
+		OPERATOR 4 >= (text, text),
+		OPERATOR 5 > (text, text);
