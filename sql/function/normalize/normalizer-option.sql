551bf2ef034d91e324b7aea5325c1f7697ecd5be
Kouhei Sutou <kou@clear-code.com>
2018-03-05 14:06:00 +0900

    normalizer test: stop to use the default normalizer name

--- sql/function/normalize/normalizer-option.sql
+++ sql/function/normalize/normalizer-option.sql
@@ -1,1 +1,2 @@
-SELECT pgroonga_normalize('aBcDe 123', 'NormalizerAuto');
+SELECT pgroonga_command('plugin_register normalizers/mysql')::json->1;
+SELECT pgroonga_normalize('aBcDe 123', 'NormalizerMySQLGeneralCI');
