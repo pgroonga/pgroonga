d57d7c094ccbeb6ffc54e14824d2b681b5c426db
Kouhei Sutou <kou@clear-code.com>
2018-03-13 18:38:22 +0900

    test: update type name

--- sql/full-text-search/text/single/match-condition-v2/weights/positive/bitmapscan.sql
+++ sql/full-text-search/text/single/match-condition-v2/weights/positive/bitmapscan.sql
@@ -17,10 +17,10 @@ SET enable_bitmapscan = on;
 EXPLAIN (COSTS OFF)
 SELECT id, content, pgroonga_score(tableoid, ctid)
   FROM memos
- WHERE content &@ ('Groonga', ARRAY[5], 'pgrn_index')::pgroonga_match_condition;
+ WHERE content &@ ('Groonga', ARRAY[5], 'pgrn_index')::pgroonga_full_text_search_condition;
 
 SELECT id, content, pgroonga_score(tableoid, ctid)
   FROM memos
- WHERE content &@ ('Groonga', ARRAY[5], 'pgrn_index')::pgroonga_match_condition;
+ WHERE content &@ ('Groonga', ARRAY[5], 'pgrn_index')::pgroonga_full_text_search_condition;
 
 DROP TABLE memos;
