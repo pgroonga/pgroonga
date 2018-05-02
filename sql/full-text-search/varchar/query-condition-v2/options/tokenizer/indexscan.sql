4a692e9fd7146e35ab39dc17c25bed170a489a73
Kouhei Sutou <kou@clear-code.com>
2018-05-02 15:54:51 +0900

    Use index internally for processing varchar &@/&@~ sequential scan

---
+++ sql/full-text-search/varchar/query-condition-v2/options/tokenizer/indexscan.sql
@@ -0,0 +1,31 @@
+CREATE TABLE memos (
+  content varchar(256)
+);
+
+INSERT INTO memos VALUES ('090-1234-5678');
+INSERT INTO memos VALUES ('(090)1234-5678');
+
+CREATE INDEX pgrn_index ON memos
+ USING pgroonga (content pgroonga_varchar_full_text_search_ops_v2)
+  WITH (tokenizer = 'TokenNgram("loose_symbol", true)');
+
+SET enable_seqscan = off;
+SET enable_indexscan = on;
+SET enable_bitmapscan = off;
+
+EXPLAIN (COSTS OFF)
+SELECT content, pgroonga_score(tableoid, ctid)
+  FROM memos
+ WHERE content &@~
+       ('090-12345678',
+        ARRAY[5],
+        'pgrn_index')::pgroonga_full_text_search_condition;
+
+SELECT content, pgroonga_score(tableoid, ctid)
+  FROM memos
+ WHERE content &@~
+       ('090-12345678',
+        ARRAY[5],
+        'pgrn_index')::pgroonga_full_text_search_condition;
+
+DROP TABLE memos;
