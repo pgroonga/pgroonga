bf51443db5e8fc69ef5416d0fd65884fc1129630
Kouhei Sutou <kou@clear-code.com>
2018-11-12 12:52:37 +0900

    pgroonga_tokenize: can keep more data

--- src/pgrn-tokenize.c
+++ src/pgrn-tokenize.c
@@ -19,6 +19,83 @@ static grn_obj tokenJSON;
 
 PGRN_FUNCTION_INFO_V1(pgroonga_tokenize);
 
+typedef struct {
+	grn_id id;
+	grn_obj data;
+	int32_t position;
+	grn_bool forcePrefix;
+	uint64_t sourceOffset;
+	uint32_t sourceLength;
+	uint32_t sourceFirstCharacterLength;
+	grn_obj metadata;
+} PGrnToken;
+
+static void
+PGrnTokensInit(void)
+{
+	GRN_VALUE_FIX_SIZE_INIT(&tokens, GRN_OBJ_VECTOR, GRN_ID_NIL);
+}
+
+static size_t
+PGrnTokensSize(void)
+{
+	return GRN_BULK_VSIZE(&tokens) / sizeof(PGrnToken);
+}
+
+static PGrnToken *
+PGrnTokensAt(size_t i)
+{
+	PGrnToken *rawTokens;
+	rawTokens = (PGrnToken *) GRN_BULK_HEAD(&tokens);
+	return rawTokens + i;
+}
+
+static void
+PGrnTokensReinit(void)
+{
+	size_t i;
+	size_t nTokens;
+
+	nTokens = PGrnTokensSize();
+	for (i = 0; i < nTokens; i++)
+	{
+		PGrnToken *token;
+		token = PGrnTokensAt(i);
+		GRN_OBJ_FIN(ctx, &(token->data));
+		GRN_OBJ_FIN(ctx, &(token->metadata));
+	}
+	GRN_BULK_REWIND(&tokens);
+}
+
+#if GRN_VERSION_OR_LATER(8, 0, 9)
+static void
+PGrnTokensAppend(grn_id id, grn_token_cursor *tokenCursor)
+{
+	PGrnToken *token;
+	grn_token *grnToken;
+	grn_obj *data;
+
+	grn_bulk_space(ctx, &tokens, sizeof(PGrnToken));
+	token = ((PGrnToken *) (GRN_BULK_CURR(&tokens))) - 1;
+	GRN_TEXT_INIT(&(token->data), 0);
+	GRN_TEXT_INIT(&(token->metadata), GRN_OBJ_VECTOR);
+
+	grnToken = grn_token_cursor_get_token(ctx, tokenCursor);
+	data = grn_token_get_data(ctx, grnToken);
+	GRN_TEXT_SET(ctx,
+				 &(token->data),
+				 GRN_TEXT_VALUE(data),
+				 GRN_TEXT_LEN(data));
+}
+#endif
+
+static void
+PGrnTokensFin(void)
+{
+	PGrnTokensReinit();
+	GRN_OBJ_FIN(ctx, &tokens);
+}
+
 void
 PGrnInitializeTokenize(void)
 {
@@ -29,7 +106,7 @@ PGrnInitializeTokenize(void)
 	GRN_TEXT_INIT(&tokenizerValue, 0);
 	GRN_TEXT_INIT(&normalizerValue, 0);
 	GRN_TEXT_INIT(&tokenFiltersValue, 0);
-	GRN_TEXT_INIT(&tokens, GRN_OBJ_VECTOR);
+	PGrnTokensInit();
 	GRN_TEXT_INIT(&tokenJSON, 0);
 }
 
@@ -37,7 +114,7 @@ void
 PGrnFinalizeTokenize(void)
 {
 	GRN_OBJ_FIN(ctx, &tokenJSON);
-	GRN_OBJ_FIN(ctx, &tokens);
+	PGrnTokensFin();
 	GRN_OBJ_FIN(ctx, &tokenFiltersValue);
 	GRN_OBJ_FIN(ctx, &normalizerValue);
 	GRN_OBJ_FIN(ctx, &tokenizerValue);
@@ -114,30 +191,20 @@ PGrnTokenize(text *target)
 										0);
 	PGrnCheck("tokenize: failed to create token cursor");
 
-	GRN_BULK_REWIND(&tokens);
+	PGrnTokensReinit();
 	while (grn_token_cursor_get_status(ctx, tokenCursor) ==
 		   GRN_TOKEN_CURSOR_DOING)
 	{
 		grn_id id = grn_token_cursor_next(ctx, tokenCursor);
-		grn_token *token;
-		grn_obj *data;
 
 		if (id == GRN_ID_NIL)
 			continue;
 
-		token = grn_token_cursor_get_token(ctx, tokenCursor);
-		data = grn_token_get_data(ctx, token);
-		grn_vector_add_element(ctx,
-							   &tokens,
-							   GRN_TEXT_VALUE(data),
-							   GRN_TEXT_LEN(data),
-							   0,
-							   GRN_DB_TEXT);
+		PGrnTokensAppend(id, tokenCursor);
 	}
-
 	grn_token_cursor_close(ctx, tokenCursor);
 
-	nTokens = grn_vector_size(ctx, &tokens);
+	nTokens = PGrnTokensSize();
 	if (nTokens == 0)
 	{
 		return construct_empty_array(JSONOID);
@@ -147,15 +214,15 @@ PGrnTokenize(text *target)
 	for (i = 0; i < nTokens; i++)
 	{
 		grn_content_type type = GRN_CONTENT_JSON;
-		const char *data;
-		unsigned int length;
+		PGrnToken *token = PGrnTokensAt(i);
 		text *json;
 
 		GRN_BULK_REWIND(&tokenJSON);
 		grn_output_map_open(ctx, &tokenJSON, type, "token", 1);
 		grn_output_cstr(ctx, &tokenJSON, type, "data");
-		length = grn_vector_get_element(ctx, &tokens, i, &data, NULL, NULL);
-		grn_output_str(ctx, &tokenJSON, type, data, length);
+		grn_output_str(ctx, &tokenJSON, type,
+					   GRN_TEXT_VALUE(&(token->data)),
+					   GRN_TEXT_LEN(&(token->data)));
 		grn_output_map_close(ctx, &tokenJSON, type);
 		json = cstring_to_text_with_len(GRN_TEXT_VALUE(&tokenJSON),
 										GRN_TEXT_LEN(&tokenJSON));
