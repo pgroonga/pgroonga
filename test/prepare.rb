d623df090112e48d31edec82008796f6c2364137
Sutou Kouhei <kou@clear-code.com>
2019-12-22 11:54:07 +0900

    test: fix schedule format

--- test/prepare.rb
+++ test/prepare.rb
@@ -9,7 +9,7 @@ Find.find("sql") do |entry|
     FileUtils.mkdir_p(results_directory)
   elsif File.file?(entry)
     next unless entry.end_with?(".sql")
-    test_file = entry.gsub(/sql\z/, "test")
-    puts(test_file)
+    test_file = entry.gsub(/\Asql\/|\.sql\z/, "")
+    puts("test: #{test_file}")
   end
 end
