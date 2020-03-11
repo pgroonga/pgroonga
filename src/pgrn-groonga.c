60ff62df08c0f711ac02e0adc5cb5fd97b17ca76
Horimoto Yasuhiro <horimoto@clear-code.com>
2020-03-11 11:01:29 +0900

    Fix a process of compare the version (#126)
    * Fix compare versions
    
    * Remove needless prototype declaration
    
    * Don't allocate
    
    Co-authored-by: Sutou Kouhei <kou@clear-code.com>

--- src/pgrn-groonga.c
+++ src/pgrn-groonga.c
@@ -16,6 +16,25 @@ bool PGrnIsTemporaryIndexSearchAvailable;
 static grn_ctx *ctx = &PGrnContext;
 static struct PGrnBuffers *buffers = &PGrnBuffers;
 
+static bool
+IsTemporaryIndexSearchAvailable(void)
+{
+	const char *libgroonga_version;
+	int major_version;
+
+	libgroonga_version = grn_get_version();
+
+	major_version = atoi(libgroonga_version);
+
+	if (major_version > 8)
+		return true;
+
+	if (major_version < 8)
+		return false;
+
+	return strcmp(libgroonga_version, "8.0.2") > 0;
+}
+
 void
 PGrnInitializeGroongaInformation(void)
 {
@@ -34,14 +53,7 @@ PGrnInitializeGroongaInformation(void)
 	GRN_BULK_REWIND(&grnIsSupported);
 	grn_obj_get_info(ctx, NULL, GRN_INFO_SUPPORT_ZSTD, &grnIsSupported);
 	PGrnIsZstdAvailable = (GRN_BOOL_VALUE(&grnIsSupported));
-
-	{
-		const char *libgroonga_version;
-		libgroonga_version = grn_get_version();
-		PGrnIsTemporaryIndexSearchAvailable =
-			(atoi(libgroonga_version) >= 8 &&
-			 (strcmp(libgroonga_version, "8.0.2") > 0));
-	}
+	PGrnIsTemporaryIndexSearchAvailable = IsTemporaryIndexSearchAvailable();
 
 	GRN_OBJ_FIN(ctx, &grnIsSupported);
 }
