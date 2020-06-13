5f7953c53bb88682cbf513910fefb20eab23dedf
Sutou Kouhei <kou@clear-code.com>
2020-06-13 17:06:40 +0900

    Add support for upgrading from 2.2.5

--- data/pgroonga_database--2.2.5--2.2.6.sql
+++ data/pgroonga_database--2.2.5--2.2.6.sql
@@ -1,1 +1,15 @@
 -- Upgrade SQL
+
+CREATE FUNCTION pgroonga_index_column_name(indexName text, columnName text)
+	RETURNS text
+	AS 'MODULE_PATHNAME', 'pgroonga_index_column_name_string'
+	LANGUAGE C
+	STABLE
+	STRICT;
+
+CREATE FUNCTION pgroonga_index_column_name(indexName text, i int4)
+	RETURNS text
+	AS 'MODULE_PATHNAME', 'pgroonga_index_column_name_int4'
+	LANGUAGE C
+	STABLE
+	STRICT;
