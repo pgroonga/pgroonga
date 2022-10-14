d743528e8058ec0aaa4ce10b00f28102b59abce2
Horimoto Yasuhiro <horimoto@clear-code.com>
2022-10-14 17:10:24 +0900

    Remove needless column definitions

--- sql/full-text-search/varchar/query-v2/partitioning/many-partition.sql
+++ sql/full-text-search/varchar/query-v2/partitioning/many-partition.sql
@@ -8,113 +8,18 @@ CREATE TABLE infection_numbers_influenza (
 )
 PARTITION BY LIST (city_code);
 
-CREATE TABLE infection_numbers_influenza_20_01 PARTITION OF infection_numbers_influenza FOR VALUES IN ('20-01') (
-    country varchar(16) NOT NULL,
-    prefecture varchar(4) NOT NULL,
-    city varchar(3) NOT NULL,
-    n_infection varchar(205),
-    city_code varchar(5) NOT NULL,
-    remarks varchar(200)
-);
-
-CREATE TABLE infection_numbers_influenza_20_02 PARTITION OF infection_numbers_influenza FOR VALUES IN ('20-02') (
-    country varchar(16) NOT NULL,
-    prefecture varchar(4) NOT NULL,
-    city varchar(3) NOT NULL,
-    n_infection varchar(205),
-    city_code varchar(5) NOT NULL,
-    remarks varchar(200)
-);
-
-CREATE TABLE infection_numbers_influenza_20_03 PARTITION OF infection_numbers_influenza FOR VALUES IN ('20-03') (
-    country varchar(16) NOT NULL,
-    prefecture varchar(4) NOT NULL,
-    city varchar(3) NOT NULL,
-    n_infection varchar(205),
-    city_code varchar(5) NOT NULL,
-    remarks varchar(200)
-);
-
-CREATE TABLE infection_numbers_influenza_20_04 PARTITION OF infection_numbers_influenza FOR VALUES IN ('20-04') (
-    country varchar(16) NOT NULL,
-    prefecture varchar(4) NOT NULL,
-    city varchar(3) NOT NULL,
-    n_infection varchar(205),
-    city_code varchar(5) NOT NULL,
-    remarks varchar(200)
-);
-
-CREATE TABLE infection_numbers_influenza_21_01 PARTITION OF infection_numbers_influenza FOR VALUES IN ('21-01') (
-    country varchar(16) NOT NULL,
-    prefecture varchar(4) NOT NULL,
-    city varchar(3) NOT NULL,
-    n_infection varchar(205),
-    city_code varchar(5) NOT NULL,
-    remarks varchar(200)
-);
-
-CREATE TABLE infection_numbers_influenza_21_02 PARTITION OF infection_numbers_influenza FOR VALUES IN ('21-02') (
-    country varchar(16) NOT NULL,
-    prefecture varchar(4) NOT NULL,
-    city varchar(3) NOT NULL,
-    n_infection varchar(205),
-    city_code varchar(5) NOT NULL,
-    remarks varchar(200)
-);
-
-CREATE TABLE infection_numbers_influenza_21_03 PARTITION OF infection_numbers_influenza FOR VALUES IN ('21-03') (
-    country varchar(16) NOT NULL,
-    prefecture varchar(4) NOT NULL,
-    city varchar(3) NOT NULL,
-    n_infection varchar(205),
-    city_code varchar(5) NOT NULL,
-    remarks varchar(200)
-);
-
-CREATE TABLE infection_numbers_influenza_21_04 PARTITION OF infection_numbers_influenza FOR VALUES IN ('21-04') (
-    country varchar(16) NOT NULL,
-    prefecture varchar(4) NOT NULL,
-    city varchar(3) NOT NULL,
-    n_infection varchar(205),
-    city_code varchar(5) NOT NULL,
-    remarks varchar(200)
-);
-
-CREATE TABLE infection_numbers_influenza_22_01 PARTITION OF infection_numbers_influenza FOR VALUES IN ('22-01') (
-    country varchar(16) NOT NULL,
-    prefecture varchar(4) NOT NULL,
-    city varchar(3) NOT NULL,
-    n_infection varchar(205),
-    city_code varchar(5) NOT NULL,
-    remarks varchar(200)
-);
-
-CREATE TABLE infection_numbers_influenza_22_02 PARTITION OF infection_numbers_influenza FOR VALUES IN ('22-02') (
-    country varchar(16) NOT NULL,
-    prefecture varchar(4) NOT NULL,
-    city varchar(3) NOT NULL,
-    n_infection varchar(205),
-    city_code varchar(5) NOT NULL,
-    remarks varchar(200)
-);
-
-CREATE TABLE infection_numbers_influenza_22_03 PARTITION OF infection_numbers_influenza FOR VALUES IN ('22-03') (
-    country varchar(16) NOT NULL,
-    prefecture varchar(4) NOT NULL,
-    city varchar(3) NOT NULL,
-    n_infection varchar(205),
-    city_code varchar(5) NOT NULL,
-    remarks varchar(200)
-);
-
-CREATE TABLE infection_numbers_influenza_22_04 PARTITION OF infection_numbers_influenza FOR VALUES IN ('22-04') (
-    country varchar(16) NOT NULL,
-    prefecture varchar(4) NOT NULL,
-    city varchar(3) NOT NULL,
-    n_infection varchar(205),
-    city_code varchar(5) NOT NULL,
-    remarks varchar(200)
-);
+CREATE TABLE infection_numbers_influenza_20_01 PARTITION OF infection_numbers_influenza FOR VALUES IN ('20-01');
+CREATE TABLE infection_numbers_influenza_20_02 PARTITION OF infection_numbers_influenza FOR VALUES IN ('20-02');
+CREATE TABLE infection_numbers_influenza_20_03 PARTITION OF infection_numbers_influenza FOR VALUES IN ('20-03');
+CREATE TABLE infection_numbers_influenza_20_04 PARTITION OF infection_numbers_influenza FOR VALUES IN ('20-04');
+CREATE TABLE infection_numbers_influenza_21_01 PARTITION OF infection_numbers_influenza FOR VALUES IN ('21-01');
+CREATE TABLE infection_numbers_influenza_21_02 PARTITION OF infection_numbers_influenza FOR VALUES IN ('21-02');
+CREATE TABLE infection_numbers_influenza_21_03 PARTITION OF infection_numbers_influenza FOR VALUES IN ('21-03');
+CREATE TABLE infection_numbers_influenza_21_04 PARTITION OF infection_numbers_influenza FOR VALUES IN ('21-04');
+CREATE TABLE infection_numbers_influenza_22_01 PARTITION OF infection_numbers_influenza FOR VALUES IN ('22-01');
+CREATE TABLE infection_numbers_influenza_22_02 PARTITION OF infection_numbers_influenza FOR VALUES IN ('22-02');
+CREATE TABLE infection_numbers_influenza_22_03 PARTITION OF infection_numbers_influenza FOR VALUES IN ('22-03');
+CREATE TABLE infection_numbers_influenza_22_04 PARTITION OF infection_numbers_influenza FOR VALUES IN ('22-04');
 
 INSERT INTO infection_numbers_influenza_20_04 VALUES ('Japan','Mie','Ise','134','20-04','age: 30, gender:male, overseas travel history: true');
 INSERT INTO infection_numbers_influenza_21_04 VALUES ('Germany','BER','BER','221','21-04','age: 50, gender:male, overseas travel history: false');
