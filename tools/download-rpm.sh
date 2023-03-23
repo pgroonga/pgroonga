2afbc3b0ced7fbe7b1925be6d23bfb811969fa7a
Horimoto Yasuhiro <horimoto@clear-code.com>
2023-03-23 14:44:22 +0900

    tools download-rpm: complete (#303)
    Co-authored-by: Sutou Kouhei <kou@clear-code.com>

--- tools/download-rpm.sh
+++ tools/download-rpm.sh
@@ -13,37 +13,96 @@ GROONGA_VERSION=$2
 POSTGRESQL_VERSION=$3
 
 OS_VERSION=$(cut -d: -f5 /etc/system-release-cpe | cut -d. -f1)
+ARCH=$(rpm -qf --queryformat="%{ARCH}" /etc/redhat-release)
 POSTGRESQL_MAJOR_VERSION=${POSTGRESQL_VERSION%%.*}
 
 if [ ${OS_VERSION} -lt 8 ]; then
   sudo yum install -y yum-utils
   DNF_DOWNLOAD="yumdownloader"
+  DNF_REPOQUERY_DEPLIST="yum deplist"
 else
   sudo dnf install -y 'dnf-command(download)'
-  DNF_DOWNLOAD="dnf download --arch x86_64"
+  DNF_DOWNLOAD="dnf download -y"
+  DNF_REPOQUERY_DEPLIST="dnf repoquery --deplist"
 fi
 
 list_dependencies()
 {
-  local target=$1
-  if [ ${OS_VERSION} -lt 8 ]; then
-    yum deplist ./${target} | \
-      grep provider: | \
-      sed -e 's/^ *provider: //g'
-  else
-  fi
+  local target="$1"
+  yum deplist "${target}" | \
+    grep provider: | \
+    sed -e 's/^ *provider: //g' | \
+    grep "\\.${ARCH}" | \
+    sed -e "s/\\.${ARCH} /-/g" \
+        -e "s/\\.${ARCH}$//g" | \
+    sort | \
+    uniq
 }
 
-# TODO: Use this to prevent infinite loop
-processed=()
+system_libraries=(
+  bash
+  glibc
+  krb5-libs
+  libcurl
+  libgcc
+  libstdc++
+  ncurses-libs
+  openssl-libs
+  postgresql${POSTGRESQL_MAJOR_VERSION}-server
+)
+
+is_system_library()
+{
+  local target="$1"
+
+  for system_library in "${system_libraries[@]}"; do
+    case "${target}" in
+      ${system_library}-*)
+        return 0
+        ;;
+    esac
+  done
+
+  return 1
+}
+
+processed_dependencies=()
 
 download_recursive()
 {
-  local target=$1
+  local target="$1"
 
-  ${DNF_DOWNLOAD} ${target}
+  echo "Downloading: ${target}"
+  ${DNF_DOWNLOAD} "${target}.${ARCH}"
+  processed_dependencies+=("${target}")
 
-  # list_dependencies ${target} | while read dependency; ...
+  for dependency in $(list_dependencies "${target}"); do
+    echo "Dependency: ${target} -> ${dependency}"
+
+    if is_system_library "${dependency}"; then
+      continue
+    fi
+
+    # Pin Groonga version
+    case "${dependency}" in
+      groonga-libs-*)
+        dependency="groonga-libs-${GROONGA_VERSION}-1.el${OS_VERSION}"
+        ;;
+    esac
+
+    local processed="false"
+    for processed_dependency in "${processed_dependencies[@]}"; do
+      if [ "${processed_dependency}" = "${dependency}" ]; then
+        processed="true"
+        break
+      fi
+    done
+    if [ "${processed}" = "true" ]; then
+      continue
+    fi
+
+    download_recursive "${dependency}"
+  done
 }
 
 download_recursive \
