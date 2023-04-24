48afe17864a436cfcbd6110e874e70a7560a548b
Sutou Kouhei <kou@clear-code.com>
2023-04-24 23:13:06 +0900

    test reindex: add a test for REINDEX CONCURRENTLY with UPDATE
    It's for 9854d197c3d3f8fd8443c442f564604780994d0d.

---
+++ test/test-reindex.rb
@@ -0,0 +1,26 @@
+require_relative "helpers/sandbox"
+
+class ReindexTestCase < Test::Unit::TestCase
+  include Helpers::Sandbox
+
+  test "REINDEX CONCURRENTLY with UPDATE" do
+    run_sql("CREATE TABLE memos (content text);")
+    run_sql("CREATE INDEX memos_content ON memos USING pgroonga (content);")
+    run_sql("INSERT INTO memos " +
+            "SELECT i::text FROM generate_series(1, 1000) AS i;")
+    3.times do |i|
+      reindex_finished = false
+      reindex_thread = Thread.new do
+        begin
+          run_sql("REINDEX INDEX CONCURRENTLY memos_content;")
+        ensure
+          reindex_finished = true
+        end
+      end
+      until reindex_finished
+        run_sql("UPDATE memos SET content = content || content;")
+      end
+      reindex_thread.join
+    end
+  end
+end
