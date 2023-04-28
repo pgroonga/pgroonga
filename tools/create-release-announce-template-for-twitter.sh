23948f18a863166d7495be47e491b3366ca49904
Horimoto Yasuhiro <horimoto@clear-code.com>
2023-04-28 13:46:30 +0900

    Use "cut" instead of "awk"
    Because this is simple.

--- tools/create-release-announce-template-for-twitter.sh
+++ tools/create-release-announce-template-for-twitter.sh
@@ -12,8 +12,8 @@ RELEASE_BLOG_VERSION="pgroonga-$PGROONGA_LATEST_VERSION"
 RELEASE_BLOG_URL_JA="$RELEASE_BLOG_BASE_URL/ja/blog/$RELEASE_BLOG_DATE_PATH/$RELEASE_BLOG_VERSION.html"
 RELEASE_BLOG_URL_EN="$RELEASE_BLOG_BASE_URL/en/blog/$RELEASE_BLOG_DATE_PATH/$RELEASE_BLOG_VERSION.html"
 
-HTTP_RESPONCE_CODE_JA=$(curl --head $RELEASE_BLOG_URL_JA | head -n 1 | awk '{print $2}')
-HTTP_RESPONCE_CODE_EN=$(curl --head $RELEASE_BLOG_URL_EN | head -n 1 | awk '{print $2}')
+HTTP_RESPONCE_CODE_JA=$(curl --head $RELEASE_BLOG_URL_JA | head -n 1 | cut -d ' ' -f 2)
+HTTP_RESPONCE_CODE_EN=$(curl --head $RELEASE_BLOG_URL_EN | head -n 1 | cut -d ' ' -f 2)
 
 if [ $HTTP_RESPONCE_CODE_JA -eq 404 ]; then
   echo "$RELEASE_BLOG_URL_JA is not found. This URL is incorrect."
