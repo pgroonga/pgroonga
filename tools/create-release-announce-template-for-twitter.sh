96adfb8274f0794e7ce1bddbd49714af1be12d80
Horimoto Yasuhiro <horimoto@clear-code.com>
2023-04-28 15:29:38 +0900

    tools: add a newly tool for creating release announce template for Twitter (#330)
    This tool output the following template.
    
    ```
    PGroonga 3.0.1: 2023-04-27 リリース！ https://groonga.org/ja/blog/2023/04/27/pgroonga-3.0.1.html 今回のリリースでは、
    PGroonga 3.0.1: 2023-04-27 has been released! https://groonga.org/en/blog/2023/04/27/pgroonga-3.0.1.html In this release,
    ```
    
    This tool get the version of PGroonga and the release date in GitHub
    release page.
    Also, this tool confirm created URL whether valid or not.
    
    ---------
    
    Co-authored-by: Sutou Kouhei <kou@clear-code.com>

---
+++ tools/create-release-announce-template-for-twitter.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -ue
+
+PGROONGA_LATEST_VERSION_AND_DATE=$(curl https://api.github.com/repos/pgroonga/pgroonga/releases/latest | jq -r '.name')
+PGROONGA_LATEST_VERSION=$(curl https://api.github.com/repos/pgroonga/pgroonga/releases/latest | jq -r '.tag_name')
+
+RELEASE_BLOG_BASE_URL="https://groonga.org"
+RELEASE_BLOG_DATE_PATH=$(echo $PGROONGA_LATEST_VERSION_AND_DATE | cut -d ' ' -f 3 | tr - /)
+RELEASE_BLOG_VERSION="pgroonga-$PGROONGA_LATEST_VERSION"
+
+RELEASE_BLOG_URL_JA="$RELEASE_BLOG_BASE_URL/ja/blog/$RELEASE_BLOG_DATE_PATH/$RELEASE_BLOG_VERSION.html"
+RELEASE_BLOG_URL_EN="$RELEASE_BLOG_BASE_URL/en/blog/$RELEASE_BLOG_DATE_PATH/$RELEASE_BLOG_VERSION.html"
+
+HTTP_RESPONCE_CODE_JA=$(curl --head $RELEASE_BLOG_URL_JA | head -n 1 | cut -d ' ' -f 2)
+HTTP_RESPONCE_CODE_EN=$(curl --head $RELEASE_BLOG_URL_EN | head -n 1 | cut -d ' ' -f 2)
+
+if [ $HTTP_RESPONCE_CODE_JA -eq 404 ]; then
+  echo "$RELEASE_BLOG_URL_JA is not found. This URL is incorrect."
+  exit 1
+elif [ $HTTP_RESPONCE_CODE_EN -eq 404 ]; then
+  echo "$RELEASE_BLOG_URL_EN is not found. This URL is incorrect."
+  exit 1
+fi
+
+echo "$PGROONGA_LATEST_VERSION_AND_DATE リリース！ $RELEASE_BLOG_URL_JA 今回のリリースでは、"
+echo "$PGROONGA_LATEST_VERSION_AND_DATE has been released! $RELEASE_BLOG_URL_EN In this release,"
