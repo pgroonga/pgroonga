927d7c6b95add8ce997d021a1f06d4999cd653be
Sutou Kouhei <kou@clear-code.com>
2023-05-31 14:51:09 +0900

    test wal_set_applied_position: add a test with declarative partitioning

---
+++ sql/function/wal-set-applied-position/declrative-partitioning.sql
@@ -0,0 +1,41 @@
+SET pgroonga.enable_wal = yes;
+
+CREATE TABLE memos (
+  id integer,
+  content text
+) PARTITION BY RANGE (id);
+
+CREATE TABLE memos_0_10
+  PARTITION OF memos
+  FOR VALUES FROM (0) TO (10);
+CREATE TABLE memos_10_20
+  PARTITION OF memos
+  FOR VALUES FROM (10) TO (20);
+CREATE TABLE memos_20_30
+  PARTITION OF memos
+  FOR VALUES FROM (20) TO (30);
+
+INSERT INTO memos
+  SELECT id, 'data: ' || id
+    FROM generate_series(0, 29) AS id;
+
+CREATE INDEX memos_fts ON memos USING pgroonga (content);
+
+SELECT pgroonga_wal_set_applied_position(0, 0);
+SELECT name, current_block, current_offset FROM pgroonga_wal_status();
+SELECT pgroonga_wal_set_applied_position();
+
+SET enable_seqscan = off;
+SET enable_indexscan = on;
+SET enable_bitmapscan = off;
+
+EXPLAIN (COSTS OFF)
+SELECT count(id)
+  FROM memos
+ WHERE content &@~ 'data 1*';
+
+SELECT count(id)
+  FROM memos
+ WHERE content &@~ 'data 1*';
+
+DROP TABLE memos;
