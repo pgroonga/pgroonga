c08d644d184df25a0c6d345136be4391d6501e2c
Sutou Kouhei <kou@clear-code.com>
2023-10-19 17:29:13 +0900

    Add missing pgroonga_snippet_html() migration for 2.4.1->2.4.2
    https://twitter.com/takadat/status/1714310734472208630
    
    Reported by takadat. Thanks!!!

--- data/pgroonga--2.4.1--2.4.2.sql
+++ data/pgroonga--2.4.1--2.4.2.sql
@@ -1,1 +1,10 @@
 -- Upgrade SQL
+
+CREATE FUNCTION pgroonga_snippet_html(target text, keywords text[], width integer DEFAULT 200)
+	RETURNS text[]
+	AS 'MODULE_PATHNAME', 'pgroonga_snippet_html'
+	LANGUAGE C
+	IMMUTABLE
+	STRICT
+	PARALLEL SAFE;
+DROP FUNCTION pgroonga_snippet_html(target text, keywords text[]);
