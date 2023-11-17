1ead30585e946b1da2dfabc57a044a80968dfcdc
Horimoto Yasuhiro <horimoto@clear-code.com>
2023-11-17 15:33:01 +0900

    Add a missing test sql

---
+++ sql/function/query-expand/specify-non-table.sql
@@ -0,0 +1,16 @@
+CREATE TABLE synonym_groups (
+  synonyms text[]
+);
+
+CREATE INDEX synonym_groups_synonyms_index
+  ON synonym_groups
+  USING pgroonga (synonyms pgroonga_text_array_term_search_ops_v2);
+
+INSERT INTO synonym_groups VALUES (ARRAY['Groonga', 'Senna']);
+
+SELECT pgroonga_query_expand('synonym_groups_synonyms_index',
+                             'synonyms',
+                             'synonyms',
+                             'groonga');
+
+DROP TABLE synonym_groups;
