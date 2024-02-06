9f32892215f5de44fed2225bb77192cc59de7617
Horimoto Yasuhiro <horimoto@clear-code.com>
2024-02-06 14:03:08 +0900

    Use more suitable name
    Co-authored-by: Sutou Kouhei <kou@clear-code.com>

--- tools/download-rpm.sh
+++ tools/download-rpm.sh
@@ -116,7 +116,7 @@ if [ ${USE_MECAB:-no} = "yes" ]; then
     groonga-tokenizer-mecab-${GROONGA_VERSION}-1.el${OS_VERSION}
 fi
 
-if [ ${USE_PLUGIN_SUGGEST:-no} = "yes" ]; then
+if [ ${USE_GROONGA_PLUGIN_SUGGEST:-no} = "yes" ]; then
   download_recursive \
     groonga-plugin-suggest-${GROONGA_VERSION}-1.el${OS_VERSION}
 fi
