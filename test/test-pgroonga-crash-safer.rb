45881b2d84ee9731aa870ba5f35748cdb52f409b
Abe Tomoaki <abe@clear-code.com>
2024-03-08 07:48:48 +0900

    Update name of tests

--- test/test-pgroonga-crash-safer.rb
+++ test/test-pgroonga-crash-safer.rb
@@ -283,21 +283,21 @@ log_autovacuum_min_duration = 0
     end
   end
 
-  test "max_recovery_threads parameter: default" do
+  test "max_recovery_threads: default" do
     pgroonga_log = @postgresql.read_pgroonga_log
     assert_equal(["pgroonga: crash-safer: max_recovery_threads: 0"],
                  pgroonga_log.scan(/pgroonga: crash-safer: max_recovery_threads:.*$/),
                  pgroonga_log)
   end
 
-  sub_test_case("max_recovery_threads parameter") do
+  sub_test_case("max_recovery_threads") do
     def additional_configurations
       super + <<-CONFIG
 pgroonga_crash_safer.max_recovery_threads = -1
       CONFIG
     end
 
-    test "set -1" do
+    test "set" do
       pgroonga_log = @postgresql.read_pgroonga_log
       assert_equal(["pgroonga: crash-safer: max_recovery_threads: -1"],
                    pgroonga_log.scan(/pgroonga: crash-safer: max_recovery_threads:.*$/),
