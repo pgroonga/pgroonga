5ce24dfc3d1bfeed5b50bbef3d99513b3690a08c
Abe Tomoaki <abe@clear-code.com>
2024-05-01 11:48:45 +0900

    Add upgrade/downgrade queries

--- data/pgroonga--3.2.0--3.2.1.sql
+++ data/pgroonga--3.2.0--3.2.1.sql
@@ -1,1 +1,8 @@
 -- Upgrade SQL
+
+CREATE FUNCTION pgroonga_list_broken_indexes()
+	RETURNS void /* todo */
+	AS 'MODULE_PATHNAME', 'pgroonga_list_broken_indexes'
+	LANGUAGE C
+	STRICT
+	PARALLEL SAFE;
