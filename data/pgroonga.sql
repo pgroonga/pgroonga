168b7deeb10f5f24316a5d0ca127b57c5af6d6ce
Abe Tomoaki <abe@clear-code.com>
2024-05-21 17:10:29 +0900

    Check the lag with pgroonga_wal_status() (#442)
    * List PGroonga indexes with unapplied WAL in pgroonga_wal_status()
    * List all PGroonga indexes if primary log position and lag

--- data/pgroonga.sql
+++ data/pgroonga.sql
@@ -434,12 +434,45 @@ CREATE FUNCTION pgroonga_wal_set_applied_position()
 	LANGUAGE C
 	STRICT;
 
-CREATE FUNCTION pgroonga_list_lagged_indexes()
-	RETURNS void /* todo */
-	AS 'MODULE_PATHNAME', 'pgroonga_list_lagged_indexes'
-	LANGUAGE C
-	STRICT
-	PARALLEL SAFE;
+DO LANGUAGE plpgsql $$
+DECLARE
+	rec record;
+BEGIN
+	-- A column in pg_stat_wal_receiver is different for 12 and 13 or higher.
+	--* received_lsn: 12
+	--* flushed_lsn: 13 or higher
+	SELECT * INTO STRICT rec from pg_settings WHERE name = 'server_version';
+	IF SUBSTR(rec.setting, 1, 2) >= '13' THEN
+		CREATE FUNCTION pgroonga_list_lagged_indexes()
+			RETURNS TABLE (name text) AS '
+				SELECT name FROM pgroonga_wal_status()
+				WHERE current_block != last_block
+					OR current_offset != current_offset
+					OR current_size != last_size
+					OR EXISTS(
+						SELECT 1 FROM pg_stat_wal_receiver
+						WHERE flushed_lsn != latest_end_lsn
+					);
+			' LANGUAGE SQL
+			STRICT
+			PARALLEL SAFE;
+	ELSE
+		CREATE FUNCTION pgroonga_list_lagged_indexes()
+			RETURNS TABLE (name text) AS '
+				SELECT name FROM pgroonga_wal_status()
+				WHERE current_block != last_block
+					OR current_offset != current_offset
+					OR current_size != last_size
+					OR EXISTS(
+						SELECT 1 FROM pg_stat_wal_receiver
+						WHERE received_lsn != latest_end_lsn
+					);
+			' LANGUAGE SQL
+			STRICT
+			PARALLEL SAFE;
+	END IF;
+END;
+$$;
 
 CREATE FUNCTION pgroonga_list_broken_indexes()
 	RETURNS SETOF text
