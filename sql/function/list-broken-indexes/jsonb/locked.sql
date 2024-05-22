33e974235a8609cab6b399c1dadb3cf8422d87d0
Abe Tomoaki <abe@clear-code.com>
2024-05-22 11:58:33 +0900

    Fix SQL style

--- sql/function/list-broken-indexes/jsonb/locked.sql
+++ sql/function/list-broken-indexes/jsonb/locked.sql
@@ -9,9 +9,9 @@ INSERT INTO logs VALUES (3, '{"message": "Good-by World"}');
 CREATE INDEX pgroonga_index ON logs
   USING pgroonga (record pgroonga_jsonb_ops_v2);
 
-select pgroonga_command(
+SELECT pgroonga_command(
   'lock_acquire',
-  ARRAY['target_name', replace(pgroonga_table_name('pgroonga_index'), 'Sources', 'JSONPaths') || '_0']
+  ARRAY['target_name', REPLACE(pgroonga_table_name('pgroonga_index'), 'Sources', 'JSONPaths') || '_0']
 )::jsonb->>1;
 
 SELECT * FROM pgroonga_list_broken_indexes();
