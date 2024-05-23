54bcfd7e4576511170ae49840bf425dd6eb7a2a3
Abe Tomoaki <abe@clear-code.com>
2024-05-23 11:23:33 +0900

    list-broken-indexes: Add tests for jsonb
    Related: GH-454

---
+++ sql/function/list-broken-indexes/jsonb/value-lexicon-number-locked.sql
@@ -0,0 +1,19 @@
+CREATE TABLE logs (
+  id int,
+  record jsonb
+);
+
+INSERT INTO logs VALUES (1, '{"message": "Hello World"}');
+INSERT INTO logs VALUES (2, '{"message": "This is a pen"}');
+INSERT INTO logs VALUES (3, '{"message": "Good-by World"}');
+CREATE INDEX pgroonga_index ON logs
+  USING pgroonga (record pgroonga_jsonb_ops_v2);
+
+SELECT pgroonga_command(
+  'lock_acquire',
+  ARRAY['target_name', REPLACE(pgroonga_table_name('pgroonga_index'), 'Sources', 'JSONValueLexiconNumber') || '_0']
+)::jsonb->>1;
+
+SELECT * FROM pgroonga_list_broken_indexes();
+
+DROP TABLE logs;
