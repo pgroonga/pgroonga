2b62f17a7f22ac25bad91be371ac2dc389011d5d
Abe Tomoaki <abe@clear-code.com>
2024-05-28 10:40:42 +0900

    pgroonga_list_lagged_indexes: Change return type

--- data/pgroonga--3.2.0--3.2.1.sql
+++ data/pgroonga--3.2.0--3.2.1.sql
@@ -10,7 +10,7 @@ BEGIN
 	SELECT * INTO STRICT rec from pg_settings WHERE name = 'server_version';
 	IF SUBSTR(rec.setting, 1, 2) >= '13' THEN
 		CREATE FUNCTION pgroonga_list_lagged_indexes()
-			RETURNS TABLE (name text) AS '
+			RETURNS SETOF text AS '
 				SELECT name FROM pgroonga_wal_status()
 				WHERE current_block != last_block
 					OR current_offset != current_offset
@@ -24,7 +24,7 @@ BEGIN
 			PARALLEL SAFE;
 	ELSE
 		CREATE FUNCTION pgroonga_list_lagged_indexes()
-			RETURNS TABLE (name text) AS '
+			RETURNS SETOF text AS '
 				SELECT name FROM pgroonga_wal_status()
 				WHERE current_block != last_block
 					OR current_offset != current_offset
