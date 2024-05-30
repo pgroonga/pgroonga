fd2b96a97a1f34fe3a8f639d6f26c4c8513d20a9
Abe Tomoaki <abe@clear-code.com>
2024-05-30 09:07:18 +0900

    Add fuzzy_max_distance_ratio parameter to pgroonga_condition()

--- data/pgroonga--3.2.1--3.2.0.sql
+++ data/pgroonga--3.2.1--3.2.0.sql
@@ -1,4 +1,27 @@
 -- Downgrade SQL
 
+ALTER TYPE pgroonga_condition DROP ATTRIBUTE fuzzy_max_distance_ratio;
+CREATE OR REPLACE FUNCTION pgroonga_condition(query text = null,
+				   weights int[] = null,
+				   scorers text[] = null,
+				   schema_name text = null,
+				   index_name text = null,
+				   column_name text = null)
+	RETURNS pgroonga_condition
+	LANGUAGE SQL
+	AS $$
+		SELECT (
+			query,
+			weights,
+			scorers,
+			schema_name,
+			index_name,
+			column_name
+		)::pgroonga_condition
+	$$
+	IMMUTABLE
+	LEAKPROOF
+	PARALLEL SAFE;
+
 DROP FUNCTION pgroonga_list_lagged_indexes;
 DROP FUNCTION pgroonga_list_broken_indexes;
