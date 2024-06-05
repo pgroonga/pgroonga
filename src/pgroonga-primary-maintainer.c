6af22e98aaf8475cf7d3e2b237b0254c177ba967
Abe Tomoaki <abe@clear-code.com>
2024-06-05 15:33:40 +0900

    pgroonga-primary-maintainer: Add a parameter specifying the threshold at which reindex is run (#478)

--- src/pgroonga-primary-maintainer.c
+++ src/pgroonga-primary-maintainer.c
@@ -1,9 +1,11 @@
 #include "pgrn-compatible.h"
 
+#include <access/relscan.h>
 #include <fmgr.h>
 #include <miscadmin.h>
 #include <postmaster/bgworker.h>
 #include <storage/ipc.h>
+#include <utils/guc.h>
 
 PG_MODULE_MAGIC;
 
@@ -13,13 +15,17 @@ extern PGDLLEXPORT void pgroonga_primary_maintainer_main(Datum datum)
 
 #define TAG "pgroonga: primary-maintainer"
 
+static int PGroongaPrimaryMaintainerReindexThreshold =
+	(1024 * 1024 * 1024) / BLCKSZ; // 1GB in size
 static const char *PGroongaPrimaryMaintainerLibraryName =
 	"pgroonga_primary_maintainer";
 
 void
 pgroonga_primary_maintainer_main(Datum arg)
 {
-	elog(LOG, TAG ": debug");
+	elog(LOG,
+		 TAG ": reindex_threshold=%d",
+		 PGroongaPrimaryMaintainerReindexThreshold);
 	proc_exit(1);
 }
 
@@ -30,6 +36,25 @@ _PG_init(void)
 
 	if (!process_shared_preload_libraries_in_progress)
 		return;
+
+	DefineCustomIntVariable(
+		"pgroonga_primary_maintainer.reindex_threshold",
+		"Specifies the number of WAL blocks. "
+		"If the specified value is exceeded, `REINDEX CONCURRENTLY` is run.",
+		"The default is 1GB in size. "
+		"This parameter specifies the number of blocks, but you can also be "
+		"specified by size. "
+		"When specifying by size, you must always add a unit. "
+		"You can use units `B`, `kB`, `MB`, `GB`, and `TB`.",
+		&PGroongaPrimaryMaintainerReindexThreshold,
+		PGroongaPrimaryMaintainerReindexThreshold,
+		1,
+		INT_MAX,
+		PGC_SIGHUP,
+		GUC_UNIT_BLOCKS,
+		NULL,
+		NULL,
+		NULL);
 
 	snprintf(worker.bgw_name, BGW_MAXLEN, TAG ": main");
 	snprintf(worker.bgw_type, BGW_MAXLEN, "%s", worker.bgw_name);
