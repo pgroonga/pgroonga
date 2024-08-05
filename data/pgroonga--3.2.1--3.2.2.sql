459d34c36a7670a9157113a038fc6c51100357e3
Abe Tomoaki <abe@clear-code.com>
2024-08-05 13:56:08 +0900

    Remove unnecessary pgroonga_condition() definitions

--- data/pgroonga--3.2.1--3.2.2.sql
+++ data/pgroonga--3.2.1--3.2.2.sql
@@ -1,1 +1,10 @@
 -- Upgrade SQL
+
+-- Two pgroonga_condition() were defined by a bug in `data/pgroonga--3.2.0--3.2.1.sql`.
+-- Remove unnecessary definitions.
+DROP FUNCTION IF EXISTS pgroonga_condition(query text,
+				   weights int[],
+				   scorers text[],
+				   schema_name text,
+				   index_name text,
+				   column_name text);
