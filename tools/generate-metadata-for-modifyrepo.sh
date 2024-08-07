251cc214b4a2ff6b5c129a94f2f7f9393ff37c6b
Abe Tomoaki <abe@clear-code.com>
2024-08-07 17:25:26 +0900

    tools: Add a script to generate YAML for use with `modifyrepo_c` command (#528)
    When you download the RPM files with `tools/download-rpm.sh` and build a
    local repository, installing `groonga-tokenizer-mecab` will result in
    the following error.
    
    ```
    No available modular metadata for modular package 'mecab-0.996-2.module_el8.6.0+3340+d764b636.x86_64', it cannot be installed on the system
    No available modular metadata for modular package 'mecab-ipadic-2.7.0.20070801-16.module_el8.6.0+3340+d764b636.x86_64', it cannot be installed on the system
    Error: No available modular metadata for modular package
    ```
    
    This error occurs because there is no metadata for the modular
    repository.
    
    Add a script to generate the YAML required when registering metadata
    with `modifyrepo_c` command.

---
+++ tools/generate-metadata-for-modifyrepo.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# This script is used when building a local repository of RPMs.
+# For modular packages, metadata for them is required.
+# Generate the YAML required when registering that metadata with `modifyrepo_c` command.
+
+set -eu
+
+RPM_FILES_DIRECTORY=${1:-.}
+
+cat <<YAML
+---
+document: modulemd-defaults
+version: 1
+data:
+  module: mecab
+  stream: "rolling"
+...
+---
+document: modulemd
+version: 2
+data:
+  name: mecab
+  stream: "rolling"
+  version: $(date "+%Y%m%d")
+  summary: "MeCab module in PGroonga local repository"
+  description: "MeCab module in PGroonga local repository"
+  license:
+    module:
+    - GPL-2.0-or-later
+    - LGPL-2.1-or-later
+    - BSD-3-Clause
+  artifacts:
+    rpms:
+YAML
+
+for rpm_file in $(find ${RPM_FILES_DIRECTORY} -name "mecab*.rpm"); do
+  echo "    - $(basename ${rpm_file} | sed 's/.rpm$//')"
+done
+
+cat <<YAML
+...
+YAML
