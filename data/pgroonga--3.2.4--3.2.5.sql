84a4c7868349d470c12c047f9735b2278347ea77
Horimoto Yasuhiro <horimoto@clear-code.com>
2024-10-31 22:48:01 +0900

    Add upgrade and downgrade files

--- data/pgroonga--3.2.4--3.2.5.sql
+++ data/pgroonga--3.2.4--3.2.5.sql
@@ -1,1 +1,23 @@
 -- Upgrade SQL
+
+CREATE FUNCTION pgroonga_regexp_text_array(targets text[], pattern text)
+	RETURNS bool
+	AS 'MODULE_PATHNAME', 'pgroonga_regexp_text_array'
+	LANGUAGE C
+	IMMUTABLE
+	STRICT
+	LEAKPROOF
+	PARALLEL SAFE
+	COST 300;
+
+CREATE OPERATOR &~ (
+	PROCEDURE = pgroonga_regexp_text_array,
+	LEFTARG = text[],
+	RIGHTARG = text,
+	RESTRICT = contsel,
+	JOIN = contjoinsel
+);
+
+CREATE OPERATOR CLASS pgroonga_text_array_regexp_ops_v2 FOR TYPE text[]
+	USING pgroonga AS
+		OPERATOR 22 &~ (text[], text);
