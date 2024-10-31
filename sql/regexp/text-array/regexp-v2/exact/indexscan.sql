7b5bf4b858acc9983cd198df5a36f27bb156bd16
Horimoto Yasuhiro <horimoto@clear-code.com>
2024-10-31 16:33:34 +0900

    Add tests for regexp search with "\A" and "\z"

---
+++ sql/regexp/text-array/regexp-v2/exact/indexscan.sql
@@ -0,0 +1,32 @@
+CREATE TABLE memos (
+  id integer,
+  content text[]
+);
+
+INSERT INTO memos
+     VALUES (1, ARRAY['PostgreSQL',
+                      'Groonga',
+                      'PGroonga']);
+
+INSERT INTO memos
+     VALUES (2, ARRAY['MySQL',
+                      'Mroonga']);
+
+CREATE INDEX pgrn_content_index
+    ON memos
+ USING pgroonga (content pgroonga_text_array_regexp_ops_v2);
+
+SET enable_seqscan = off;
+SET enable_indexscan = on;
+SET enable_bitmapscan = off;
+
+EXPLAIN (COSTS OFF)
+SELECT *
+  FROM memos
+ WHERE content &~ '\Agroonga\z';
+
+SELECT *
+  FROM memos
+ WHERE content &~ '\Agroonga\z';
+
+DROP TABLE memos;
