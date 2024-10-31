428b56f78886223166f1a3e205e81a1ca555c2a5
Horimoto Yasuhiro <horimoto@clear-code.com>
2024-10-31 16:26:06 +0900

    Add tests for regexp search with "."

---
+++ sql/regexp/text-array/regexp-v2/dot/seqscan.sql
@@ -0,0 +1,32 @@
+CREATE TABLE memos (
+  id integer,
+  content text[]
+);
+
+INSERT INTO memos
+     VALUES (1, ARRAY['PostgreSQL is a RDBMS',
+                      'Groonga is fast full text search engine',
+                      'PGroonga is a PostgreSQL extension that uses Groonga']);
+
+INSERT INTO memos
+     VALUES (2, ARRAY['MySQL is a RDBMS',
+                      'Mroonga is a MySQL storage engine that uses Groonga']);
+
+CREATE INDEX pgrn_content_index
+    ON memos
+ USING pgroonga (content pgroonga_text_array_regexp_ops_v2);
+
+SET enable_seqscan = on;
+SET enable_indexscan = off;
+SET enable_bitmapscan = off;
+
+EXPLAIN (COSTS OFF)
+SELECT *
+  FROM memos
+ WHERE content &~ '.torage';
+
+SELECT *
+  FROM memos
+ WHERE content &~ '.torage';
+
+DROP TABLE memos;
