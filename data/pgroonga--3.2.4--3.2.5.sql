dd5f5f136346b4f073056e3939938ad7f86094fa
Horimoto Yasuhiro <horimoto@clear-code.com>
2024-11-13 17:08:34 +0900

    Fix addition order

--- data/pgroonga--3.2.4--3.2.5.sql
+++ data/pgroonga--3.2.4--3.2.5.sql
@@ -1,5 +1,9 @@
 -- Upgrade SQL
 
+CREATE OPERATOR CLASS pgroonga_text_array_regexp_ops_v2 FOR TYPE text[]
+	USING pgroonga AS
+		OPERATOR 22 &~ (text[], text);
+
 CREATE FUNCTION pgroonga_regexp_text_array(targets text[], pattern text)
 	RETURNS bool
 	AS 'MODULE_PATHNAME', 'pgroonga_regexp_text_array'
@@ -18,6 +22,3 @@ CREATE OPERATOR &~ (
 	JOIN = contjoinsel
 );
 
-CREATE OPERATOR CLASS pgroonga_text_array_regexp_ops_v2 FOR TYPE text[]
-	USING pgroonga AS
-		OPERATOR 22 &~ (text[], text);
