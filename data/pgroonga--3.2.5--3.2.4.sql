74b85aeba65606c31d9adf4069e5dbaa20dacba1
Horimoto Yasuhiro <horimoto@clear-code.com>
2024-11-13 14:38:54 +0900

    Fix wrong dropping order

--- data/pgroonga--3.2.5--3.2.4.sql
+++ data/pgroonga--3.2.5--3.2.4.sql
@@ -1,5 +1,5 @@
 -- Downgrade SQL
 
 DROP FUNCTION pgroonga_regexp_text_array;
+DROP OPERATOR CLASS pgroonga_text_array_regexp_ops_v2 USING pgroonga;
 DROP OPERATOR FAMILY pgroonga_text_array_regexp_ops_v2 USING pgroonga;
-DROP OPERATOR CLASS pgroonga_text_array_regexp_ops_v2;
