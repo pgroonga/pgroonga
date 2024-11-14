73dfbd3910994a338dccd312a12ac2b8f7ff5e2f
Horimoto Yasuhiro <horimoto@clear-code.com>
2024-11-14 21:47:24 +0900

    Remove needless DROP
    Because PostgreSQL also remove OPERATOR CLASS when we remove OPERATOR FAMILY.

--- data/pgroonga--3.2.5--3.2.4.sql
+++ data/pgroonga--3.2.5--3.2.4.sql
@@ -1,6 +1,5 @@
 -- Downgrade SQL
 
 DROP OPERATOR FAMILY pgroonga_text_array_regexp_ops_v2 USING pgroonga;
-DROP OPERATOR CLASS pgroonga_text_array_regexp_ops_v2 USING pgroonga;
 DROP OPERATOR &~ (text[], text);
 DROP FUNCTION pgroonga_regexp_text_array;
