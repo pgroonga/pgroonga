3082e0c68c13e54a91a922b5112ed0196b90054d
Horimoto Yasuhiro <horimoto@clear-code.com>
2024-11-23 21:18:00 +0900

    Remove needless column

--- sql/regexp/text-array/regexp-condition-v2/null/indexscan.sql
+++ sql/regexp/text-array/regexp-condition-v2/null/indexscan.sql
@@ -1,16 +1,15 @@
 CREATE TABLE memos (
-  id integer,
   content text[]
 );
 
 INSERT INTO memos
-     VALUES (1, ARRAY['PostgreSQL is a RDBMS',
-                      'Groonga is fast full text search engine',
-                      'PGroonga is a PostgreSQL extension that uses Groonga']);
+     VALUES (ARRAY['PostgreSQL is a RDBMS',
+                   'Groonga is fast full text search engine',
+                   'PGroonga is a PostgreSQL extension that uses Groonga']);
 
 INSERT INTO memos
-     VALUES (2, ARRAY['MySQL is a RDBMS',
-                      'Mroonga is a MySQL storage engine that uses Groonga']);
+     VALUES (ARRAY['MySQL is a RDBMS',
+                   'Mroonga is a MySQL storage engine that uses Groonga']);
 
 CREATE INDEX pgrn_content_index
     ON memos
