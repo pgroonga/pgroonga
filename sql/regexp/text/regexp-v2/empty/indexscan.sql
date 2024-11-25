264c08973ad53ccd017e4c9b23efd1dca97d5bd8
Horimoto Yasuhiro <horimoto@clear-code.com>
2024-11-25 15:25:06 +0900

    Use more suitable index name

--- sql/regexp/text/regexp-v2/empty/indexscan.sql
+++ sql/regexp/text/regexp-v2/empty/indexscan.sql
@@ -6,7 +6,7 @@ INSERT INTO memos VALUES ('PostgreSQL is a RDBMS');
 INSERT INTO memos VALUES ('Groonga is fast full text search engine');
 INSERT INTO memos VALUES ('PGroonga is a PostgreSQL extension that uses Groonga');
 
-CREATE INDEX grnindex ON memos
+CREATE INDEX pgrn_content_index ON memos
   USING pgroonga (content pgroonga_text_regexp_ops_v2);
 
 SET enable_seqscan = off;
