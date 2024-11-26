316ebcb89373aecf9a998c6b31a836b6a4ea238d
Horimoto Yasuhiro <horimoto@clear-code.com>
2024-11-26 15:51:18 +0900

    regexp-v2 text[]: add tests for NULL pattern

---
+++ sql/regexp/text-array/regexp-v2/null/seqscan.sql
@@ -0,0 +1,27 @@
+CREATE TABLE memos (
+  contents text[]
+);
+
+INSERT INTO memos
+     VALUES (ARRAY['PostgreSQL is a RDBMS',
+                   'Groonga is fast full text search engine',
+                   'PGroonga is a PostgreSQL extension that uses Groonga']);
+
+INSERT INTO memos
+     VALUES (ARRAY['MySQL is a RDBMS',
+                   'Mroonga is a MySQL storage engine that uses Groonga']);
+
+SET enable_seqscan = on;
+SET enable_indexscan = off;
+SET enable_bitmapscan = off;
+
+EXPLAIN (COSTS OFF)
+SELECT *
+  FROM memos
+ WHERE contents &~ NULL;
+
+SELECT *
+  FROM memos
+ WHERE contents &~ NULL;
+
+DROP TABLE memos;
