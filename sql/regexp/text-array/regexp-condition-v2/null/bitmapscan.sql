3ad7734bc200f3a6b0b62e02fc9bb2e4c3447004
Horimoto Yasuhiro <horimoto@clear-code.com>
2024-11-26 11:26:27 +0900

    Add tests for NULL pattern

--- sql/regexp/text-array/regexp-condition-v2/null/bitmapscan.sql
+++ sql/regexp/text-array/regexp-condition-v2/null/bitmapscan.sql
@@ -15,6 +15,16 @@ CREATE INDEX pgrn_content_index
     ON memos
  USING pgroonga (content pgroonga_text_array_regexp_ops_v2);
 
+SET enable_seqscan = off;
+SET enable_indexscan = off;
+SET enable_bitmapscan = on;
+
+EXPLAIN (COSTS OFF)
+SELECT *
+  FROM memos
+ WHERE content &~ pgroonga_condition(NULL,
+                                     index_name => 'pgrn_content_index');
+
 SELECT *
   FROM memos
  WHERE content &~ pgroonga_condition(NULL,
