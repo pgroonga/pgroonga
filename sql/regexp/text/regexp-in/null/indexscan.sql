8bf5be306e43c71299c9f30c3f9504fefa596294
Horimoto Yasuhiro <horimoto@clear-code.com>
2024-11-28 15:10:04 +0900

    Remove needless records

--- sql/regexp/text/regexp-in/null/indexscan.sql
+++ sql/regexp/text/regexp-in/null/indexscan.sql
@@ -3,8 +3,6 @@ CREATE TABLE memos (
 );
 
 INSERT INTO memos VALUES ('PostgreSQL is a RDBMS');
-INSERT INTO memos VALUES ('Groonga is fast full text search engine');
-INSERT INTO memos VALUES ('PGroonga is a PostgreSQL extension that uses Groonga');
 
 CREATE INDEX pgrn_content_index ON memos
   USING pgroonga (content pgroonga_text_regexp_ops_v2);
