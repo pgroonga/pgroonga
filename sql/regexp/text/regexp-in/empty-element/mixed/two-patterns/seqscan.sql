a38926e38cc235df5934733424e82832efc05b5c
Horimoto Yasuhiro <horimoto@clear-code.com>
2024-11-28 15:22:31 +0900

    regexp-in text: add tests for empty pattern with sequential search

---
+++ sql/regexp/text/regexp-in/empty-element/mixed/two-patterns/seqscan.sql
@@ -0,0 +1,22 @@
+CREATE TABLE memos (
+  content text
+);
+
+INSERT INTO memos VALUES ('PostgreSQL is a RDBMS');
+INSERT INTO memos VALUES ('Groonga is fast full text search engine');
+INSERT INTO memos VALUES ('PGroonga is a PostgreSQL extension that uses Groonga');
+
+SET enable_seqscan = on;
+SET enable_indexscan = off;
+SET enable_bitmapscan = off;
+
+EXPLAIN (COSTS OFF)
+SELECT *
+  FROM memos
+ WHERE content &~| ARRAY['', 'rdbms\z', '\Agroonga'];
+
+SELECT *
+  FROM memos
+ WHERE content &~| ARRAY['', 'rdbms\z', '\Agroonga'];
+
+DROP TABLE memos;
