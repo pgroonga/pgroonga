0c026d828af2a3885770007d483a2debac7ecbdc
takuya kodama <otegami@clear-code.com>
2024-11-28 13:10:30 +0900

    pgroonga_list_lagged_indexes(): fix the offset condtion (#616)
    GitHub: GH-615
    
    Fix the offset condition because the previous condition always returned
    true.

--- data/pgroonga--3.2.4--3.2.5.sql
+++ data/pgroonga--3.2.4--3.2.5.sql
@@ -41,3 +41,17 @@ CREATE OPERATOR CLASS pgroonga_text_array_regexp_ops_v2 FOR TYPE text[]
 	USING pgroonga AS
 		OPERATOR 22 &~ (text[], text),
                 OPERATOR 47 &~ (text[], pgroonga_condition);
+
+CREATE OR REPLACE FUNCTION pgroonga_list_lagged_indexes()
+	RETURNS SETOF text AS '
+		SELECT name FROM pgroonga_wal_status()
+		WHERE current_block != last_block
+			OR current_offset != last_offset
+			OR current_size != last_size
+			OR EXISTS(
+				SELECT 1 FROM pg_stat_wal_receiver
+				WHERE flushed_lsn != latest_end_lsn
+			);
+	' LANGUAGE SQL
+	STRICT
+	PARALLEL SAFE;
--- data/pgroonga--3.2.5--3.2.4.sql
+++ data/pgroonga--3.2.5--3.2.4.sql
@@ -5,3 +5,17 @@ DROP OPERATOR &~ (text[], text);
 DROP OPERATOR &~ (text[], pgroonga_condition);
 DROP FUNCTION pgroonga_regexp_text_array;
 DROP FUNCTION pgroonga_regexp_text_array_condition;
+
+CREATE OR REPLACE FUNCTION pgroonga_list_lagged_indexes()
+	RETURNS SETOF text AS '
+		SELECT name FROM pgroonga_wal_status()
+		WHERE current_block != last_block
+			OR current_offset != current_offset
+			OR current_size != last_size
+			OR EXISTS(
+				SELECT 1 FROM pg_stat_wal_receiver
+				WHERE flushed_lsn != latest_end_lsn
+			);
+	' LANGUAGE SQL
+	STRICT
+	PARALLEL SAFE;
