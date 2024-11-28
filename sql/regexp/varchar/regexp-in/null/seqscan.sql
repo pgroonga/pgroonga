6fec165996dadfccf58c973982486e8a7fcac31f
Horimoto Yasuhiro <horimoto@clear-code.com>
2024-11-28 17:23:49 +0900

    Remove needless records

--- sql/regexp/varchar/regexp-in/null/seqscan.sql
+++ sql/regexp/varchar/regexp-in/null/seqscan.sql
@@ -3,8 +3,6 @@ CREATE TABLE memos (
 );
 
 INSERT INTO memos VALUES ('PostgreSQL is a RDBMS');
-INSERT INTO memos VALUES ('Groonga is fast full text search engine');
-INSERT INTO memos VALUES ('PGroonga is a PostgreSQL extension that uses Groonga');
 
 SET enable_seqscan = on;
 SET enable_indexscan = off;
