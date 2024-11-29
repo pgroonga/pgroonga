8db6cacb7d4702c2b3c8af6624b9492a6e6d27ef
Horimoto Yasuhiro <horimoto@clear-code.com>
2024-11-29 09:27:25 +0900

    regexp-in text: add check for NULL (#619)

---
+++ sql/regexp/text/regexp-in/null/indexscan.sql
@@ -0,0 +1,23 @@
+CREATE TABLE memos (
+  content text
+);
+
+INSERT INTO memos VALUES ('PostgreSQL is a RDBMS');
+
+CREATE INDEX pgrn_content_index ON memos
+  USING pgroonga (content pgroonga_text_regexp_ops_v2);
+
+SET enable_seqscan = off;
+SET enable_indexscan = on;
+SET enable_bitmapscan = off;
+
+EXPLAIN (COSTS OFF)
+SELECT *
+  FROM memos
+ WHERE content &~| NULL;
+
+SELECT *
+  FROM memos
+ WHERE content &~| NULL;
+
+DROP TABLE memos;
