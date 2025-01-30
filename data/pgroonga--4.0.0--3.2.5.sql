991bd867786f4151b9c6f3748937a471f0d6f6e8
Horimoto Yasuhiro <horimoto@clear-code.com>
2025-01-30 14:54:31 +0900

    Remove needless condition
    Because Postgres-XL is a dead project.

--- data/pgroonga--4.0.0--3.2.5.sql
+++ data/pgroonga--4.0.0--3.2.5.sql
@@ -1,23 +1,12 @@
 -- Downgrade SQL
 
-DO LANGUAGE plpgsql $$
-BEGIN
-	PERFORM setting FROM pg_settings
-	 WHERE name = 'server_version' AND
-	       setting LIKE '%Postgres-XL%';
-	-- "CREATE SCHEMA pgroonga" and CREATE FUNCTION pgroonga.XXX" don't work
-	-- on Postgres-XL.
-	IF NOT FOUND THEN
-		/* v1 */
-		CREATE OPERATOR CLASS pgroonga.text_full_text_search_ops FOR TYPE text
-			USING pgroonga AS
-				OPERATOR 6 ~~,
-				OPERATOR 7 ~~*,
-				OPERATOR 8 %%,
-				OPERATOR 9 @@,
-				OPERATOR 12 &@,
-				OPERATOR 13 &?, -- For backward compatibility
-				OPERATOR 28 &@~;
-	END IF;
-END;
-$$;
+/* v1 */
+CREATE OPERATOR CLASS pgroonga.text_full_text_search_ops FOR TYPE text
+    USING pgroonga AS
+        OPERATOR 6 ~~,
+        OPERATOR 7 ~~*,
+        OPERATOR 8 %%,
+        OPERATOR 9 @@,
+        OPERATOR 12 &@,
+        OPERATOR 13 &?, -- For backward compatibility
+        OPERATOR 28 &@~;
