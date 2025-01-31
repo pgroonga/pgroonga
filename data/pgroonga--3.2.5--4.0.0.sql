0c70e57c1e034713d72d3ad99610b483b90ff247
Horimoto Yasuhiro <horimoto@clear-code.com>
2025-01-31 18:26:31 +0900

    data: remove pgroonga.varchar_full_text_search_ops operator class
    GitHub: GH-647
    
    This is part of the task of remove "pgroonga" schema. It's deprecated since PGroonga 2.
    This is incompatible with PGroonga 3.x or earlier.

--- data/pgroonga--3.2.5--4.0.0.sql
+++ data/pgroonga--3.2.5--4.0.0.sql
@@ -2,3 +2,4 @@
 
 DROP OPERATOR FAMILY pgroonga.text_full_text_search_ops USING pgroonga;
 DROP OPERATOR FAMILY pgroonga.text_array_full_text_search_ops USING pgroonga;
+DROP OPERATOR FAMILY pgroonga.varchar_full_text_search_ops USING pgroonga;
--- data/pgroonga.sql
+++ data/pgroonga.sql
@@ -3133,13 +3133,14 @@ BEGIN
 			PARALLEL SAFE;
 
 		/* v1 */
-		CREATE OPERATOR CLASS pgroonga.varchar_full_text_search_ops FOR TYPE varchar
+		CREATE OPERATOR CLASS pgroonga.text_array_full_text_search_ops
+			FOR TYPE text[]
 			USING pgroonga AS
-				OPERATOR 8 %%,
-				OPERATOR 9 @@,
-				OPERATOR 12 &@,
-				OPERATOR 13 &?, -- For backward compatibility
-				OPERATOR 28 &@~;
+				OPERATOR 8 %% (text[], text),
+				OPERATOR 9 @@ (text[], text),
+				OPERATOR 12 &@ (text[], text),
+				OPERATOR 13 &? (text[], text), -- For backward compatibility
+				OPERATOR 28 &@~ (text[], text);
 
 		CREATE OPERATOR CLASS pgroonga.varchar_ops FOR TYPE varchar
 			USING pgroonga AS
