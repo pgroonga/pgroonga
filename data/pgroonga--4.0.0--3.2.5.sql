78c84c93a4b0a2113554c0c1e11a5fd09ba50ff1
Horimoto Yasuhiro <horimoto@clear-code.com>
2025-02-05 22:22:47 +0900

    pgroonga: remove pgroonga.highlight_html(target text, keywords text[]) function
    This is part of the task of remove "pgroonga" schema. It's deprecated since PGroonga 2.
    This is incompatible with PGroonga 3.x or earlier.
    
    PGroonga's test don't remove in this PR.
    Because "pgroonga.highlight_html(target text, keywords text[])" is not used in PGroonga's tests as below.
    
    ```
    % grep -r "pgroonga\.highlight_html" ./*
    ./data/pgroonga--3.2.5.sql:		CREATE FUNCTION pgroonga.highlight_html(target text, keywords text[])
    ./data/pgroonga--1.0.6--1.0.7.sql:CREATE FUNCTION pgroonga.highlight_html(target text, keywords text[])
    ./data/pgroonga--4.0.0--3.2.5.sql:CREATE FUNCTION pgroonga.highlight_html(target text, keywords text[])
    ./data/pgroonga--4.0.0.sql:		CREATE FUNCTION pgroonga.highlight_html(target text, keywords text[])
    ./data/pgroonga--3.2.5--4.0.0.sql:DROP FUNCTION pgroonga.highlight_html(target text, keywords text[]);
    ./src/pgrn-highlight-html.c: * pgroonga.highlight_html(target text, keywords text[]) : text
    ./src/pgrn-highlight-html.c: * pgroonga.highlight_html(target text, keywords text[], indexName cstring) :
    ./src/pgrn-highlight-html.c: * pgroonga.highlight_html(target text[], keywords text[]) : text[]
    ./src/pgrn-highlight-html.c: * pgroonga.highlight_html(target text[], keywords text[], indexName cstring) :
    ```
    
    TODO:
    * Test
    * Resolve confrict

--- data/pgroonga--4.0.0--3.2.5.sql
+++ data/pgroonga--4.0.0--3.2.5.sql
@@ -195,3 +195,11 @@ CREATE OPERATOR CLASS pgroonga.varchar_regexp_ops_v2 FOR TYPE varchar
     USING pgroonga AS
         OPERATOR 10 @~, -- For backward compatibility
         OPERATOR 22 &~;
+
+CREATE FUNCTION pgroonga.highlight_html(target text, keywords text[])
+    RETURNS text
+    AS 'MODULE_PATHNAME', 'pgroonga_highlight_html'
+    LANGUAGE C
+    IMMUTABLE
+    STRICT
+    PARALLEL SAFE;
--- data/pgroonga.sql
+++ data/pgroonga.sql
@@ -2665,14 +2665,6 @@ BEGIN
 			STRICT
 			PARALLEL SAFE;
 
-		CREATE FUNCTION pgroonga.highlight_html(target text, keywords text[])
-			RETURNS text
-			AS 'MODULE_PATHNAME', 'pgroonga_highlight_html'
-			LANGUAGE C
-			IMMUTABLE
-			STRICT
-			PARALLEL SAFE;
-
 		CREATE FUNCTION pgroonga.match_positions_byte(target text, keywords text[])
 			RETURNS integer[2][]
 			AS 'MODULE_PATHNAME', 'pgroonga_match_positions_byte'
