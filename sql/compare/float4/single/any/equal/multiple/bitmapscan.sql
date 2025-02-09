890a8826c5d45a2d0ebbe7cb7880f0166e69885b
Horimoto Yasuhiro <horimoto@clear-code.com>
2025-02-09 13:24:09 +0900

    pgroonga_float4_ops: fix a bug that we can't specify pgroonga_float4_ops in index (#665)
    PGroonga reports the following error when we specify
    `pgroonga_float4_ops` in index with Groonga 10.0.2 or later.
    
    ```sql
    CREATE TABLE ids (
      id real
    );
    
    INSERT INTO ids VALUES (2.1);
    
    CREATE INDEX grnindex ON ids USING pgroonga (id pgroonga_float4_ops);
    ERROR:  pgroonga: [insert] failed to set column value: [column][fix][set-value][BuildingSources31238.id] too long value: <8>: max:<4>
    ```
    
    ---------
    
    Co-authored-by: Sutou Kouhei <kou@clear-code.com>

---
+++ sql/compare/float4/single/any/equal/multiple/bitmapscan.sql
@@ -0,0 +1,33 @@
+CREATE TABLE ids (
+  id real
+);
+
+INSERT INTO ids VALUES (2.1);
+INSERT INTO ids VALUES (7.1);
+INSERT INTO ids VALUES (6.1);
+INSERT INTO ids VALUES (4.1);
+INSERT INTO ids VALUES (5.1);
+INSERT INTO ids VALUES (8.1);
+INSERT INTO ids VALUES (1.1);
+INSERT INTO ids VALUES (10.1);
+INSERT INTO ids VALUES (3.1);
+INSERT INTO ids VALUES (9.1);
+
+CREATE INDEX pgroonga_index ON ids USING pgroonga (id pgroonga_float4_ops);
+
+SET enable_seqscan = off;
+SET enable_indexscan = off;
+SET enable_bitmapscan = on;
+
+EXPLAIN (COSTS OFF)
+SELECT id
+  FROM ids
+ WHERE id = ANY(ARRAY[6.1, 1.1, 7.1]::real[])
+ ORDER BY id ASC;
+
+SELECT id
+  FROM ids
+ WHERE id = ANY(ARRAY[6.1, 1.1, 7.1]::real[])
+ ORDER BY id ASC;
+
+DROP TABLE ids;
---
+++ sql/compare/float4/single/in/null/seqscan.sql
@@ -0,0 +1,33 @@
+CREATE TABLE ids (
+  id real
+);
+
+INSERT INTO ids VALUES (2.1);
+INSERT INTO ids VALUES (7.1);
+INSERT INTO ids VALUES (6.1);
+INSERT INTO ids VALUES (4.1);
+INSERT INTO ids VALUES (5.1);
+INSERT INTO ids VALUES (8.1);
+INSERT INTO ids VALUES (1.1);
+INSERT INTO ids VALUES (10.1);
+INSERT INTO ids VALUES (3.1);
+INSERT INTO ids VALUES (9.1);
+
+CREATE INDEX pgroonga_index ON ids USING pgroonga (id pgroonga_float4_ops);
+
+SET enable_seqscan = on;
+SET enable_indexscan = off;
+SET enable_bitmapscan = off;
+
+EXPLAIN (COSTS OFF)
+SELECT id
+  FROM ids
+ WHERE id IN ((6.1::real), NULL, (7.1::real))
+ ORDER BY id ASC;
+
+SELECT id
+  FROM ids
+ WHERE id IN ((6.1::real), NULL, (7.1::real))
+ ORDER BY id ASC;
+
+DROP TABLE ids;
