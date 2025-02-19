4e1378187325d975fe9a791d28b8d35927996c3f
Horimoto Yasuhiro <horimoto@clear-code.com>
2025-02-19 12:14:41 +0900

    Optimaize to "match in" from "&@" with "OR" and "in_values()" from "&=" or "=" with "OR"
    PostgreSQL 18 optimaize to "match in" from "column &@ keyword1 OR column &@ keyword2 OR ..."
    and optimaize to "in_values()" from "column &=/= keyword1 OR column &=/= keyword2 OR ...".
    
    So, we should handle "column &@ keyword1 OR column &@ keyword2 OR ..." as "match in" operator
    and "column &=/= keyword1 OR column &=/= keyword2 OR ..." as "in_values()" function.

---
+++ sql/term-search/text/or/equal/indexscan.sql
@@ -0,0 +1,29 @@
+CREATE TABLE tags (
+  id int,
+  name text
+);
+
+INSERT INTO tags VALUES (1, 'PostgreSQL');
+INSERT INTO tags VALUES (2, 'Groonga');
+INSERT INTO tags VALUES (3, 'groonga');
+INSERT INTO tags VALUES (4, 'PGroonga');
+
+CREATE INDEX pgrn_index ON tags
+  USING pgroonga (name pgroonga_text_term_search_ops_v2);
+
+SET enable_seqscan = off;
+SET enable_indexscan = on;
+SET enable_bitmapscan = off;
+
+EXPLAIN (COSTS OFF)
+SELECT name
+  FROM tags
+ WHERE name = 'groonga' OR name = 'pgroonga'
+ ORDER BY id;
+
+SELECT name
+  FROM tags
+ WHERE name = 'groonga' OR name = 'pgroonga'
+ ORDER BY id;
+
+DROP TABLE tags;
