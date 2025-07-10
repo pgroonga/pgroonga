a0ca2383d3edb4c0172535638ad6b1b228693dda
otegami <a.s.takuya1026@gmail.com>
2025-07-10 19:53:06 +0800

    prepare and execute test using Ruby

--- test/prepare.rb
+++ test/prepare.rb
@@ -3,13 +3,29 @@
 require "find"
 require "fileutils"
 
-Find.find("sql") do |entry|
-  if File.directory?(entry)
-    results_directory = entry.gsub(/\Asql/, "results")
-    FileUtils.mkdir_p(results_directory)
-  elsif File.file?(entry)
-    next unless entry.end_with?(".sql")
-    test_file = entry.gsub(/\Asql\/|\.sql\z/, "")
-    puts("test: #{test_file}")
+def open_output(output_path)
+  if output_path
+    File.open(output_path, "w") do |output|
+      yield(output)
+    end
+  else
+    yield($stdout)
+  end
+end
+
+open_output(ARGV[0]) do |output|
+  Find.find("sql") do |entry|
+    if File.directory?(entry)
+      results_directory = entry.gsub(/\Asql/, "results")
+      build_dir = ENV["BUILD_DIR"]
+      if build_dir
+        results_directory = File.join(build_dir, results_directory)
+      end
+      FileUtils.mkdir_p(results_directory)
+    elsif File.file?(entry)
+      next unless entry.end_with?(".sql")
+      test_file = entry.gsub(/\Asql\/|\.sql\z/, "")
+      output.puts("test: #{test_file}")
+    end
   end
 end
