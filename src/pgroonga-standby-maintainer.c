adab1002b583c39d06114de8b66de1c87a8023bc
Abe Tomoaki <abe@clear-code.com>
2025-07-22 10:48:29 +0900

    Support for both `pg_attribute_noreturn()` and `pg_noreturn` environments (#763)
    `pg_attribute_noreturn()` has been changed to `pg_noreturn` in
    PostgreSQL18.
    
    https://github.com/postgres/postgres/commit/3691edfab97187789b8a1cbb9dce4acf0ecd8f5a
    
    From a maintainability standpoint, we will take the approach of defining
    dummies and putting #if in one place.

--- src/pgroonga-standby-maintainer.c
+++ src/pgroonga-standby-maintainer.c
@@ -19,12 +19,12 @@
 PG_MODULE_MAGIC;
 
 extern PGDLLEXPORT void _PG_init(void);
-extern PGDLLEXPORT void pgroonga_standby_maintainer_apply_wal(Datum datum)
-	pg_attribute_noreturn();
-extern PGDLLEXPORT void pgroonga_standby_maintainer_maintain(Datum datum)
-	pg_attribute_noreturn();
-extern PGDLLEXPORT void pgroonga_standby_maintainer_main(Datum datum)
-	pg_attribute_noreturn();
+extern PGDLLEXPORT pg_noreturn void
+pgroonga_standby_maintainer_apply_wal(Datum datum) pg_attribute_noreturn();
+extern PGDLLEXPORT pg_noreturn void
+pgroonga_standby_maintainer_maintain(Datum datum) pg_attribute_noreturn();
+extern PGDLLEXPORT pg_noreturn void
+pgroonga_standby_maintainer_main(Datum datum) pg_attribute_noreturn();
 
 #define TAG "pgroonga: standby-maintainer"
 
