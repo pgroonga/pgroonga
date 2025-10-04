3067d86b33e797a8165586bad2c2518784349612
Abe Tomoaki <abe@clear-code.com>
2025-10-04 13:07:45 +0900

    custom-scan: Support ScalarArrayOpExpr
    By using ScanKeyEntryInitialize() instead of ScanKeyInit(), we can also set `flags`.

---
+++ sql/compare/float4/single/any/equal/multiple/custom-scan.sql
@@ -0,0 +1,36 @@
+CREATE TABLE ids (
+  id real
+);
+
+INSERT INTO ids VALUES (2.1);
+INSERT INTO ids VALUES (7.1);
+INSERT INTO ids VALUES (6.1);
+INSERT INTO ids VALUES (4.1);
+INSERT INTO ids VALUES (5.1);
+INSERT INTO ids VALUES (8.1);
+INSERT INTO ids VALUES (1.1);
+INSERT INTO ids VALUES (10.1);
+INSERT INTO ids VALUES (3.1);
+INSERT INTO ids VALUES (9.1);
+
+CREATE INDEX pgroonga_index ON ids USING pgroonga (id pgroonga_float4_ops);
+
+SET pgroonga.enable_custom_scan = on;
+
+/*
+ * todo
+ * Support sorting by index.
+ * If supported, the EXPLAIN results will change.
+ */
+EXPLAIN (COSTS OFF)
+SELECT id
+  FROM ids
+ WHERE id = ANY(ARRAY[6.1, 1.1, 7.1]::real[])
+ ORDER BY id ASC;
+
+SELECT id
+  FROM ids
+ WHERE id = ANY(ARRAY[6.1, 1.1, 7.1]::real[])
+ ORDER BY id ASC;
+
+DROP TABLE ids;
