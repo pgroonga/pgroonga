503bf8748966a7e1bbff1adb50621b918c4183b0
otegami <otegami@clear-code.com>
2025-10-09 10:29:39 +0900

    use the different column values because it's easier to debug

--- sql/term-search/text/equal-v2/row-level-security/seqscan.sql
+++ sql/term-search/text/equal-v2/row-level-security/seqscan.sql
@@ -7,7 +7,7 @@ CREATE TABLE tags (
 CREATE USER alice NOLOGIN;
 GRANT ALL ON TABLE tags TO alice;
 
-INSERT INTO tags VALUES (1, 'nonexistent', 'groonga');
+INSERT INTO tags VALUES (1, 'nonexistent', 'GROONGA');
 INSERT INTO tags VALUES (2, 'alice', 'Groonga');
 INSERT INTO tags VALUES (3, 'alice', 'groonga');
 INSERT INTO tags VALUES (4, 'alice', 'PGroonga');
