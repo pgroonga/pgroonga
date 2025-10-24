f2b876c112eaa2a836e3cea52d716f66de17129d
Sutou Kouhei <kou@clear-code.com>
2025-10-24 12:15:58 +0900

    test run-sql-test: rewrite for Meson based build

--- test/run-sql-test.sh
+++ test/run-sql-test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+source_dir=$(cd $(dirname $0) && pwd)
+top_source_dir=${source_dir}/..
+
+: ${BUILD_DIR:=$(pwd)}
+
 run()
 {
   "$@"
@@ -16,6 +21,13 @@ while [ $# -gt 0 ]; do
   case "${arg}" in
     sql/*.sql)
       test_name=$(echo "${arg}" | sed -E -e 's,^sql/|\.sql,,g')
+      expected_dir=${top_source_dir}/expected/$(dirname ${test_name})
+      expected_path=${expected_dir}/$(basename ${test_name}).out
+      if [ ! -f ${expected_path} ]; then
+        mkdir -p ${expected_dir}
+        touch ${expected_path}
+      fi
+      mkdir -p ${BUILD_DIR}/results/$(dirname ${test_name})
       test_names="${test_names[@]} ${test_name}"
       ;;
     *)
@@ -31,54 +43,37 @@ while [ $# -gt 0 ]; do
   esac
 done
 
-if [ "${SUPPRESS_LOG:-yes}" = "yes" ]; then
-  OUTPUT="> /dev/null"
-else
-  OUTPUT=""
-fi
-
-: ${PG_CONFIG:=pg_config}
-
-n_jobs=1
-case $(uname) in
-  Linux)
-    n_jobs=$(nproc)
-    ;;
-  Darwin)
-    n_jobs=$(sysctl -n hw.logicalcpu)
-    ;;
-esac
-
-export PGRN_DEBUG=1
-export HAVE_MSGPACK=1
-eval "run make -j${n_jobs} PG_CONFIG=${PG_CONFIG} ${OUTPUT}"
+meson compile -C ${BUILD_DIR}
 if [ "${NEED_SUDO:-no}" = "yes" ]; then
-  eval "run sudo -H make PG_CONFIG=${PG_CONFIG} install ${OUTPUT}"
+  sudo -H meson install -C ${BUILD_DIR} --no-rebuild
 else
-  eval "run make PG_CONFIG=${PG_CONFIG} install ${OUTPUT}"
+  meson install -C ${BUILD_DIR} --no-rebuild
 fi
 PG_REGRESS_DIFF_OPTS="-u"
 if diff --help | grep -q color; then
   PG_REGRESS_DIFF_OPTS="${PG_REGRESS_DIFF_OPTS} --color=always"
 fi
 export PG_REGRESS_DIFF_OPTS
-extra_regress_opts=""
-extra_regress_opts+=" --launcher=$(pwd)/test/short-pgappname"
+extra_regress_opts=()
 if [ -n "${TEMP_INSTANCE:-}" ]; then
-  extra_regress_opts+=" --temp-instance=${TEMP_INSTANCE}"
+  extra_regress_opts+=("--temp-instance=${TEMP_INSTANCE}")
 fi
 if [ -n "${test_names}" ]; then
-  make installcheck \
-       EXTRA_REGRESS_OPTS="${extra_regress_opts}" \
-       PG_CONFIG="${PG_CONFIG}" \
-       REGRESS="${test_names}"
+  pg_regress=$(pg_config --pkglibdir)/pgxs/src/test/regress/pg_regress
+  ${pg_regress} \
+    --bindir $(pg_config --bindir) \
+    --dlpath ${BUILD_DIR} \
+    --inputdir ${top_source_dir} \
+    --outputdir ${BUILD_DIR} \
+    --launcher ${source_dir}/short-pgappname \
+    --load-extension pgroonga \
+    "${extra_regress_opts[@]}" \
+    ${test_names}
 else
-  make installcheck \
-       EXTRA_REGRESS_OPTS="${extra_regress_opts}" \
-       PG_CONFIG="${PG_CONFIG}"
+  meson test -C ${BUILD_DIR}
 fi
 success=$?
 if [ $success != 0 ]; then
-  cat regression.diffs
+  cat ${BUILD_DIR}/regression.diffs
 fi
 exit $success
