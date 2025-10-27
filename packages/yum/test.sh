eb2dab438a8e6e1f74d1a9809a669f48fa10ebc8
Horimoto Yasuhiro <horimoto@clear-code.com>
2025-10-27 17:39:46 +0900

    Add the comment about we can remove this workaround

--- packages/yum/test.sh
+++ packages/yum/test.sh
@@ -91,6 +91,9 @@ pgroonga_package=$(basename $(ls ${packages_dir}/*-pgroonga-*.rpm | head -n1) | 
                      sed -e 's/-pgroonga-.*$/-pgroonga/g')
 postgresql_version=$(echo ${pgroonga_package} | grep -E -o '[0-9.]+')
 
+# This condition is workaround.
+# We can remove when the following issue resolved.
+# https://github.com/pgdg-packaging/pgdg-rpms/issues/105
 if [ "${os}" = "almalinux" ] && [ ${major_version} = "10" ]; then
     ${DNF} --disablerepo=epel install -y postgresql${postgresql_version}-contrib
 else
