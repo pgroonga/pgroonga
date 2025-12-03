075e8bfe271f1b71f1ae44249ec134d19d583f65
Abe Tomoaki <abe@clear-code.com>
2025-12-03 11:46:25 +0900

    Add a test

---
+++ sql/semantic-search/text/similar-condition-v2/options/prefix.sql
@@ -0,0 +1,39 @@
+-- Only test when `PGRN_LANGUAGE_MODEL_TEST` is set.
+\getenv language_model_test PGRN_LANGUAGE_MODEL_TEST
+SELECT NOT :{?language_model_test} AS omit \gset
+\if :omit
+  \quit
+\endif
+
+CREATE TABLE memos (
+  id integer,
+  content text
+);
+
+INSERT INTO memos VALUES (1, 'PostgreSQL is a RDBMS.');
+INSERT INTO memos VALUES (2, 'Groonga is fast full text search engine.');
+INSERT INTO memos VALUES (3, 'PGroonga is a PostgreSQL extension that uses Groonga.');
+
+CREATE INDEX pgrn_index ON memos
+ USING pgroonga (content pgroonga_text_semantic_search_ops_v2)
+ WITH (plugins = 'language_model/knn',
+       model = 'hf:///groonga/multilingual-e5-base-Q4_K_M-GGUF',
+       passage_prefix = 'passage: ',
+       query_prefix = 'query: ');
+
+SET enable_seqscan = off;
+SET enable_indexscan = on;
+SET enable_bitmapscan = off;
+
+EXPLAIN (COSTS OFF)
+SELECT id, content
+  FROM memos
+ WHERE content &@* pgroonga_condition('What is a MySQL alternative?')
+ ORDER BY pgroonga_score(tableoid, ctid) DESC;
+
+SELECT id, content
+  FROM memos
+ WHERE content &@* pgroonga_condition('What is a MySQL alternative?')
+ ORDER BY pgroonga_score(tableoid, ctid) DESC;
+
+DROP TABLE memos;
