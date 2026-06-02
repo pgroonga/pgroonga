5a2c83b5e37454735f3ec2e73f647a16d45bd836
Horimoto Yasuhiro <horimoto@clear-code.com>
2026-06-02 14:19:27 +0900

    Use more suitable test

--- sql/full-text-search/text/options/lexicon-flags-mapping/large.sql
+++ sql/full-text-search/text/options/lexicon-flags-mapping/large.sql
@@ -1,16 +1,26 @@
 CREATE TABLE memos (
+  title text,
   content text
 );
 
 CREATE INDEX pgrn_index ON memos
-  USING pgroonga (content)
+  USING pgroonga (title, content)
   WITH (lexicon_flags_mapping = '{
-	  "pgrn_index": ["LARGE"]
+	  "content": ["LARGE"]
 	}');
 
-SELECT entry->>3 AS flags
-  FROM json_array_elements(pgroonga_command('table_list')::json#>'{1}')
-       AS entry
- WHERE entry->>1 = 'Lexicon' || 'pgrn_index'::regclass::oid || '_0';
+\out null
+SELECT
+  'Lexicon' || 'pgrn_index'::regclass::oid || '_0' as title_lexicon_name,
+  'Lexicon' || 'pgrn_index'::regclass::oid || '_1' as content_lexicon_name;
+\out
+\gset
+
+SELECT pgroonga_command('schema')::jsonb#>'{1,tables}'
+         ->:'title_lexicon_name'
+         #>'{command,arguments,flags}';
+SELECT pgroonga_command('schema')::jsonb#>'{1,tables}'
+         ->:'content_lexicon_name'
+         #>'{command,arguments,flags}';
 
 DROP TABLE memos;
